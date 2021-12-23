87da61bb6d236a3b7c02a13647db4e61bf85d8b4
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-23 14:09:45 +0900

    レイアウトの変更

--- src/pages/aaa/test.tsx
+++ src/pages/aaa/test.tsx
@@ -3,14 +3,12 @@ import Link from "next/link";
 
 const Test: NextPage = () => {
   return (
-    <>
-      <main className="flex justify-center items-center flex-col min-h-screen ">
-        <p className="font-mono text-red-700">テスト</p>
-        <Link href="/">
-          <a className="underline hover:text-blue-800 mt-3">トップへ戻る</a>
-        </Link>
-      </main>
-    </>
+    <div className='flex flex-col items-center mt-10'>
+      <p className="font-mono text-red-700">テスト</p>
+      <Link href="/">
+        <a className="underline hover:text-blue-800 mt-3">トップへ戻る</a>
+      </Link>
+    </div>
   );
 };
 
