cb3b49c1ca5236bc7640410e08636254017a4394
koda-momo <momoko.kodama@rakus-partners.co.jp>
2021-12-24 14:26:07 +0900

    タイムライン部分のモック状態作成

--- src/pages/timeline/index.tsx
+++ src/pages/timeline/index.tsx
@@ -1,9 +1,26 @@
 import type { NextPage } from "next";
 // import Link from "next/link";
 // import { Button } from "../components/Button";
+import Image from "next/image";
 
 const Timeline: NextPage = () => {
-  return <></>;
+  const style = {
+    borderBottom: "solid 1px black",
+  };
+
+  return (
+    <>
+      <table>
+        <tr style={style}>
+          <td>
+            <Image src="/usakus.jpg" width={50} height={50} alt="icon" />
+          </td>
+          <td>佐藤花子</td>
+          <td>ああああああああああああ</td>
+        </tr>
+      </table>
+    </>
+  );
 };
 
 export default Timeline;
