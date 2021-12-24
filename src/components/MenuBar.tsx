c7194968b9fa4837afbcaf091599073f6f90a22c
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2021-12-24 15:30:04 +0900

    とりあえずのメニューサイドバー作成

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -4,26 +4,35 @@ import Link from "next/link";
 export const MenuBar: FC = memo(() => {
   return (
     <>
-      <ul>
-        <li>
-          <Link href="/aaa/test">HOME</Link>
-        </li>
-        <li>
-          <Link href="/aaa/test">プロフィール</Link>
-        </li>
-        <li>
-          <Link href="/aaa/test">ランチ</Link>
-        </li>
-        <li>
-          <Link href="/aaa/test">通知</Link>
-        </li>
-        <li>
-          <Link href="/aaa/test">ラーセン内情報</Link>
-        </li>
-        <li>
-          <Link href="/aaa/test">ログアウト</Link>
-        </li>
-      </ul>
+      <aside className="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl">
+        <nav className="text-white text-base font-semibold bg-basic pt-3">
+          <Link href="/aaa/test">
+            <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+              HOME
+            </a>
+          </Link>
+          <Link href="/aaa/test">
+            <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+              プロフィール
+            </a>
+          </Link>
+          <Link href="/aaa/test">
+            <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+              通知
+            </a>
+          </Link>
+          <Link href="/aaa/test">
+            <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+              ラーセン内情報
+            </a>
+          </Link>
+          <Link href="/aaa/test">
+            <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+              ログアウト
+            </a>
+          </Link>
+        </nav>
+      </aside>
     </>
   );
 });
