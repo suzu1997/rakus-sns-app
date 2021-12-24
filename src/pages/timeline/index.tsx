fdafe62b0c1fc2dccd47b49a9f15fa922604ca45
koda-momo <momoko.kodama@rakus-partners.co.jp>
2021-12-24 14:18:19 +0900

    timelineフォルダ／indexファイル作成

---
+++ src/pages/timeline/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+// import Link from "next/link";
+// import { Button } from "../components/Button";
+
+const Timeline: NextPage = () => {
+  return <></>;
+};
+
+export default Timeline;
