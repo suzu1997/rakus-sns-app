9dda79e3493eac6b7ae94aadc14f66ddac39108a
koda-momo <momoko.kodama@rakus-partners.co.jp>
2021-12-24 14:05:14 +0900

    footer完成

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,12 @@
+/**
+ * footer.
+ */
 import { memo, FC } from "react";
 
 export const Footer: FC = memo(() => {
-  const style = {
-    display: "flex",
-    justifyContent: "center",
-    alignItem: "center",
-  };
-
   return (
-    <footer
-      className="h-12 w-full shadow-md bg-basic text-sm text-gray-600"
-      style={style}
-    >
-      @ranchkus
+    <footer className="h-12 w-full shadow-md bg-basic text-sm text-gray-600 flex justify-center items-center">
+      ©lunchkus
     </footer>
   );
 });
