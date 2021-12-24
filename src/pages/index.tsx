6ee71b4106ec1d21bdb5a8bd079ce119d42bb071
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2021-12-24 16:18:58 +0900

    メニューサイドバー一旦完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,27 +7,31 @@ const Home: NextPage = () => {
   return (
     <>
       {" "}
-      <MenuBar></MenuBar>
-      <div className="flex flex-col items-center mt-10">
-        <p className="font-mono text-text-brown">ランチックスを作るよ テスト</p>
-        <Button
-          label="ボタン"
-          onClick={() => {
-            alert("ボタンが押されました");
-          }}
-        />
-        <Button
-          backgroundColor="#f6f0ea"
-          color="#622d18"
-          label="SubButton"
-          onClick={() => {
-            alert("サブ");
-          }}
-          size="md"
-        />
-        <Link href="/aaa/test">
-          <a className="underline hover:text-blue-800 mt-3">リンクです</a>
-        </Link>
+      <div className="flex">
+        <MenuBar></MenuBar>
+        <div className="flex-col items-center mt-10 flex">
+          <p className="font-mono text-text-brown">
+            ランチックスを作るよ テスト
+          </p>
+          <Button
+            label="ボタン"
+            onClick={() => {
+              alert("ボタンが押されました");
+            }}
+          />
+          <Button
+            backgroundColor="#f6f0ea"
+            color="#622d18"
+            label="SubButton"
+            onClick={() => {
+              alert("サブ");
+            }}
+            size="md"
+          />
+          <Link href="/aaa/test">
+            <a className="underline hover:text-blue-800 mt-3">リンクです</a>
+          </Link>
+        </div>
       </div>
     </>
   );
