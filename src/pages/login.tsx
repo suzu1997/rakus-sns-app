5f09050fe811609e1e053f056e848f66f269932a
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-01-03 15:54:52 +0900

    ログイン画面を新規作成

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,65 @@
+import { TextInput } from "../components/TextInput";
+import { useState, useCallback } from "react";
+import { Button } from "../components/Button";
+import Router from "next/router";
+
 const Login = () => {
+  const [eMail, setEMail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const inputValue = useCallback((e) => {
+    setEMail(e.target.value);
+    setPassword(e.target.value);
+  }, []);
+
+  //ログインボタンを押した時に呼ばれる
+  const submitForm = () => {
+    Router.push("/");
+  };
+  //クリアボタンを押した時に呼ばれる
+  const formClear = () => {
+    setEMail("");
+    setPassword("");
+  };
+
   return (
-    <div>
-      <h1>ログイン</h1>
+    <div className="flex flex-col items-center mt-10">
+      <div className="w-96 mt-3">
+        <TextInput
+          label="メールアドレス"
+          value={eMail}
+          type="text"
+          fullWidth={true}
+          required
+          onChange={inputValue}
+        />
+      </div>
+      <div className="w-96 mt-3">
+        <TextInput
+          label="パスワード"
+          value={password}
+          type="password"
+          fullWidth={true}
+          required
+          onChange={inputValue}
+        />
+      </div>
+      <div className="flex gap-3 mt-10">
+        <Button
+          label="ログイン"
+          backgroundColor="#f28728"
+          color="white"
+          size="md"
+          onClick={submitForm}
+        />
+        <Button
+          label="クリア"
+          backgroundColor="#f6f0ea"
+          color="#f28728"
+          size="md"
+          onClick={formClear}
+        />
+      </div>
     </div>
   );
 };
