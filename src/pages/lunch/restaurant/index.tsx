e2cec330a3d14eca18991b1a737bc628ed582f44
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-04 14:02:45 +0900

    仮のレビュー一覧ページとお店一覧ページ用ファイルを作成

---
+++ src/pages/lunch/restaurant/index.tsx
@@ -0,0 +1,25 @@
+import { NextPage } from "next";
+import Link from "next/link";
+import { LunchSearchArea } from "../../../components/LunchSearchArea";
+import LunchTabList from "../../../components/LunchTabList";
+import { MenuBar } from "../../../components/MenuBar";
+
+const LunchRestaurantList: NextPage = () => {
+  return (
+    <div className="flex">
+      <MenuBar />
+      <div className="flex-1 my-5 px-8">
+        <h1 className="text-3xl">近くのランチ</h1>
+        <Link href="/lunch/review" passHref>
+          <a className="underline">レビュー一覧へ</a>
+        </Link>
+        <div className="flex justify-between gap-8 flex-col-reverse items-center sm:flex-row sm:items-start">
+          <LunchTabList />
+          <LunchSearchArea />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LunchRestaurantList;
