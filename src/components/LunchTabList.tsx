ee6dd961a6c528e1cb584526e95391e73e11d512
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-04 14:01:24 +0900

    ランチ一覧ページ用のタブを作成

---
+++ src/components/LunchTabList.tsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { Tab } from "@headlessui/react";
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+/**
+ * ランチのレビューと店情報を表示するタブ.
+ */
+export default function LunchTabList() {
+  const [categories] = useState({
+     ランチレビュー: [
+      {
+        id: 1,
+        title: "Does drinking coffee make you smarter?",
+        date: "5h ago",
+        commentCount: 5,
+        shareCount: 2,
+      },
+      {
+        id: 2,
+        title: "So you've bought coffee... now what?",
+        date: "2h ago",
+        commentCount: 3,
+        shareCount: 2,
+      },
+    ],
+    お店情報: [
+      {
+        id: 1,
+        title: "Is tech making coffee better or worse?",
+        date: "Jan 7",
+        commentCount: 29,
+        shareCount: 16,
+      },
+      {
+        id: 2,
+        title: "The most innovative things happening in coffee",
+        date: "Mar 19",
+        commentCount: 24,
+        shareCount: 12,
+      },
+    ],
+  });
+
+  return (
+    <div className="w-full px-2 sm:px-0">
+      <Tab.Group>
+        <Tab.List className="flex p-1 space-x-1 bg-bgc rounded-xl">
+          {Object.keys(categories).map((category) => (
+            <Tab
+              key={category}
+              className={({ selected }) =>
+                classNames(
+                  "w-full py-2.5 text-sm leading-5 font-bold text-text-brown rounded-lg",
+                  "focus:outline-none focus:ring-2 ring-offset-2 ring-white ring-opacity-60",
+                  selected
+                    ? "bg-white shadow border-4 border-basic"
+                    : "text-text-brown text-opacity-60 hover:text-opacity-100 hover:bg-white hover:bg-opacity-60 ",
+                )
+              }
+            >
+              {category}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels className="mt-2">
+          {Object.values(categories).map((posts, idx) => (
+            <Tab.Panel
+              key={idx}
+              className={classNames(
+                "bg-white rounded-xl p-3",
+                "focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60",
+              )}
+            >
+              <ul>
+                {posts.map((post) => (
+                  <li
+                    key={post.id}
+                    className="relative p-3 rounded-md hover:bg-coolGray-100"
+                  >
+                    <h3 className="text-sm font-medium leading-5">
+                      {post.title}
+                    </h3>
+
+                    <ul className="flex mt-1 space-x-1 text-xs font-normal leading-4 text-coolGray-500">
+                      <li>{post.date}</li>
+                      <li>&middot;</li>
+                      <li>{post.commentCount} comments</li>
+                      <li>&middot;</li>
+                      <li>{post.shareCount} shares</li>
+                    </ul>
+
+                    <a
+                      href="#"
+                      className={classNames(
+                        "absolute inset-0 rounded-md",
+                        "focus:z-10 focus:outline-none focus:ring-2 ring-blue-400",
+                      )}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  );
+}
