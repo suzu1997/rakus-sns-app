f728f87eb4c4188ed643c5751bd7c3c55428e66f
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-04 14:14:18 +0900

    検索エリアのオプションを変数に入れ、初期値の指定方法を変更した

--- src/components/LunchSearchArea.tsx
+++ src/components/LunchSearchArea.tsx
@@ -5,9 +5,57 @@ import { SelectBox } from "./SelectBox";
  * ランチの並び替えと絞り込みを行うエリア.
  */
 export const LunchSearchArea: FC = () => {
-  const [order, setOrder] = useState("最新順");
-  const [genre, setGenre] = useState("すべて");
-  const [type, setType] = useState("すべて");
+  // 並び替えのオプション
+  const orderOptions = [
+    {
+      id: "1",
+      name: "最新順",
+    },
+    {
+      id: "2",
+      name: "評価順",
+    },
+  ];
+  // ランチのジャンルのオプション
+  const genreOptions = [
+    {
+      id: "1",
+      name: "すべて",
+    },
+    {
+      id: "2",
+      name: "中華",
+    },
+    {
+      id: "3",
+      name: "イタリアン",
+    },
+    {
+      id: "4",
+      name: "うどん",
+    },
+  ];
+  // タイプ(店内かお弁当か)のオプション
+  const typeOptions = [
+    {
+      id: "1",
+      name: "すべて",
+    },
+    {
+      id: "2",
+      name: "店内飲食",
+    },
+    {
+      id: "3",
+      name: "お弁当",
+    },
+  ];
+  // 選択中の並び替え
+  const [order, setOrder] = useState(orderOptions[0].name);
+   // 選択中のジャンル
+  const [genre, setGenre] = useState(genreOptions[0].name);
+  // 選択中のタイプ
+  const [type, setType] = useState(typeOptions[0].name);
 
   return (
     <div className="bg-bgc w-full sm:w-96 p-5 rounded-lg">
@@ -15,58 +63,19 @@ export const LunchSearchArea: FC = () => {
       <div className="flex flex-col gap-3">
         <SelectBox
           label="並び替え"
-          options={[
-            {
-              id: "1",
-              name: "最新順",
-            },
-            {
-              id: "2",
-              name: "評価順",
-            },
-          ]}
+          options={orderOptions}
           value={order}
           select={setOrder}
         />
         <SelectBox
           label="ジャンル"
-          options={[
-            {
-              id: "1",
-              name: "すべて",
-            },
-            {
-              id: "2",
-              name: "中華",
-            },
-            {
-              id: "3",
-              name: "イタリアン",
-            },
-            {
-              id: "4",
-              name: "うどん",
-            },
-          ]}
+          options={genreOptions}
           value={genre}
           select={setGenre}
         />
         <SelectBox
           label="タイプ"
-          options={[
-            {
-              id: "1",
-              name: "すべて",
-            },
-            {
-              id: "2",
-              name: "店内飲食",
-            },
-            {
-              id: "3",
-              name: "お弁当",
-            },
-          ]}
+          options={typeOptions}
           value={type}
           select={setType}
         />
