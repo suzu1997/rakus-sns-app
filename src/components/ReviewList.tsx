fefcaf414216c1eb68d9acc6f93ebb90fc58456f
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-05 08:40:29 +0900

    レビュー一覧のUIを作成

---
+++ src/components/ReviewList.tsx
@@ -0,0 +1,30 @@
+import { FC, useState } from "react";
+import { ReviewCard } from "./reviewCard";
+
+export const ReviewList: FC = () => {
+  //テストデータ
+  const [reviewData] = useState([
+    { id: 1, name: "佐藤花子", content: "あああ", img: "/usakus.jpg" },
+    { id: 2, name: "山田太郎", content: "いいい", img: "/usakus.jpg" },
+    { id: 3, name: "ランチックス", content: "ううう", img: "/usakus.jpg" },
+    { id: 4, name: "佐藤花子", content: "あああ", img: "/usakus.jpg" },
+    {
+      id: 5,
+      name: "山田太郎",
+      content:
+        "あああああああああああいいいいいいいいううううううううううえええええええええええええおおおおおおおおおおおおおおおうひうひひょひょほほほほほほほほほほほほほああああああああああああいいいいいいいいいいいいいいいいいいいいいいいいいいいいい",
+      img: "/usakus.jpg",
+    },
+    { id: 6, name: "ランチックス", content: "ううう", img: "/usakus.jpg" },
+  ]);
+
+  return (
+    <div className="w-full">
+      {reviewData.map((review) => (
+        <div key={review.id}>
+          <ReviewCard {...review} />
+        </div>
+      ))}
+    </div>
+  );
+};
