e448df9f487ff4396adda7a101ede8b2209ef6c9
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-01-05 14:59:02 +0900

    ラーセン基本情報を記載

--- src/pages/learcen/info.tsx
+++ src/pages/learcen/info.tsx
@@ -1,12 +1,60 @@
 import { NextPage } from "next";
 import { MenuBar } from "../../components/MenuBar";
+import { SubHeader } from "../../components/SubHeader";
+import Image from "next/image";
 
 const Info: NextPage = () => {
   return (
     <>
       <div className="flex">
-        <MenuBar />
-        <h1>ラーセン内情報</h1>
+        <div>
+          <MenuBar />
+        </div>
+        <div className="w-full">
+          <SubHeader title="ラーセン基本情報" />
+          <div className="border-solid  border-2 border-bgc-200 m-5 shadow-lg rounded-md text-center">
+            <div className="mt-3 text-xl font-bold">第二ラーニングセンター</div>
+            <Image
+              src="/usakus.jpg"
+              width={100}
+              height={100}
+              alt="icon"
+            ></Image>
+
+            <div className="mb-5">
+              <div>住所:東京都新宿区</div>
+              <div>解放時間:毎週土曜日9:00-18:00</div>
+            </div>
+          </div>
+          <div className="border-solid  border-2 border-bgc-200 m-5 shadow-lg rounded-md text-center">
+            <div className="mt-3 text-xl font-bold">第一ラーニングセンター</div>
+            <Image
+              src="/usakus.jpg"
+              width={100}
+              height={100}
+              alt="icon"
+            ></Image>
+
+            <div className="mb-5">
+              <div>住所:東京都新宿区</div>
+              <div>解放時間:毎週土曜日24h</div>
+            </div>
+          </div>
+          <div className="border-solid  border-2 border-bgc-200 m-5 shadow-lg rounded-md text-center">
+            <div className="mt-3 text-xl font-bold">本社</div>
+            <Image
+              src="/usakus.jpg"
+              width={100}
+              height={100}
+              alt="icon"
+            ></Image>
+
+            <div className="mb-5">
+              <div>住所:東京都新宿区</div>
+              <div>解放時間:年中無休</div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
