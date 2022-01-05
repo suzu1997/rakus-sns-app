5de0852e7d67a13a95f787817410d6e4d2d7144e
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-05 22:18:00 +0900

    inputコンポーネントで、ラベルをクリックした時に対応するinputにフォーカスを当てるよう修正

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -11,13 +11,20 @@ export type Props = {
 };
 
 export const TextInput: FC<Props> = (props) => {
-  const { label, value, type, fullWidth = true, placeholder, required, onChange } =
-    props;
+  const {
+    label,
+    value,
+    type,
+    fullWidth = true,
+    placeholder,
+    required,
+    onChange,
+  } = props;
 
   return (
     <div>
       <div className="flex items-center mb-1">
-        <label>{label}</label>
+        <label htmlFor={label}>{label}</label>
         {/* requiredがtrueの場合は必須のバッジ表示 */}
         {required && (
           <span className="bg-basic text-white text-xs font-medium ml-3 px-2 py-1.5 rounded-lg dark:bg-red-200 dark:text-red-900">
@@ -26,6 +33,7 @@ export const TextInput: FC<Props> = (props) => {
         )}
       </div>
       <input
+        id={label}
         value={value}
         type={type}
         placeholder={placeholder}
