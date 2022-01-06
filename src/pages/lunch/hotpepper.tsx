39593edee1e7f688261f160cbade4f4e704e1566
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-06 17:49:05 +0900

    ホットペッパーAPIのテストページ更新

--- src/pages/lunch/hotpepper.tsx
+++ src/pages/lunch/hotpepper.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { Button } from "../../components/Button";
 import { MenuBar } from "../../components/MenuBar";
 import { TextInput } from "../../components/TextInput";
+import { HOTPEPPER_URL } from "../../utils/const";
 
 const Hotpepper: FC = () => {
   // 店名で検索するキーワード
@@ -14,9 +15,7 @@ const Hotpepper: FC = () => {
   }, []);
 
   // ホットペッパー検索テスト
-  // API のURL
-  const URL =
-    "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=d358529c334d432a&format=json";
+
   // 検索結果
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [result, setResult] = useState<Array<any>>([]);
@@ -27,7 +26,7 @@ const Hotpepper: FC = () => {
    * &middle_area=Y055で新宿で絞り込める
    */
   const searchInShinjuku = async () => {
-    const res = await axios.get(`${URL}&middle_area=Y055&count=50`);
+    const res = await axios.get(`${HOTPEPPER_URL}&middle_area=Y055&count=50`);
     console.log(res.data.results);
     console.log(res.data.results.shop);
 
@@ -40,11 +39,10 @@ const Hotpepper: FC = () => {
    */
   const searchIn2km = async () => {
     const res = await axios.get(
-      `${URL}&lat=35.689445&lng=139.70735&range=3&count=50`,
+      `${HOTPEPPER_URL}&lat=35.689445&lng=139.70735&range=3&count=50`,
     );
     console.log(res.data.results);
     console.log(res.data.results.shop);
-    console.log(res.data.results.shop[0].photo.pc.s);
 
     setResult(res.data.results.shop);
   };
@@ -56,7 +54,7 @@ const Hotpepper: FC = () => {
    */
   const searchByNameInIn2km = async () => {
     const res = await axios.get(
-      `${URL}&name_any=${searchName}&lat=35.689445&lng=139.70735&range=3&count=50`,
+      `${HOTPEPPER_URL}&name_any=${searchName}&lat=35.689445&lng=139.70735&range=3&count=50`,
     );
     console.log(res.data.results);
     console.log(res.data.results.shop);
@@ -77,9 +75,11 @@ const Hotpepper: FC = () => {
           required={true}
           onChange={inputRestaurantName}
         />
-        <Button label="店名で検索(1km以内)" onClick={searchByNameInIn2km} />
-        <Button label="新宿内を検索" onClick={searchInShinjuku} />
-        <Button label="ラーセン(第2)から1km以内" onClick={searchIn2km} />
+        <div className="mt-10 flex gap-3">
+          <Button label="店名で検索(1km以内)" onClick={searchByNameInIn2km} />
+          <Button label="新宿内を検索" onClick={searchInShinjuku} />
+          <Button label="ラーセン(第2)から1km以内" onClick={searchIn2km} />
+        </div>
         <div className="mt-10">
           <p>検索結果(最大50件)</p>
           {result && (
@@ -91,12 +91,24 @@ const Hotpepper: FC = () => {
                       {shop.name}({shop.name_kana})
                     </li>
                     <p className="ml-10">-住所: {shop.address}</p>
-                    <p className="ml-10">-緯度: {shop.lat} 経度: {shop.lng}</p>
+                    <p className="ml-10">
+                      -緯度: {shop.lat} 経度: {shop.lng}
+                    </p>
                     <p className="ml-10">-ジャンル: {shop.genre.name}</p>
                     <p className="ml-10">-お店キャッチ: {shop.catch}</p>
                     <p className="ml-10">-交通アクセス: {shop.access}</p>
-                    <p className="ml-10">-店舗URL: {shop.urls.pc}</p>
-                    <Image src={shop.photo.pc.s} alt="image" width={150} height={100}/>
+                    <p className="ml-10">
+                      -店舗URL:{" "}
+                      <a href={shop.urls.pc} className="hover:text-blue-700">
+                        {shop.urls.pc}
+                      </a>
+                    </p>
+                    <Image
+                      src={shop.photo.pc.m}
+                      alt="image"
+                      width={150}
+                      height={100}
+                    />
                   </div>
                 );
               })}
