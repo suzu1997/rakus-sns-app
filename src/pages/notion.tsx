1b88d8086dbf74e28beea6b4874801e7f513554a
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-07 14:35:55 +0900

    HTML完成

--- src/pages/notion.tsx
+++ src/pages/notion.tsx
@@ -13,23 +13,33 @@ const Notion: NextPage = () => {
   //テストデータ
   //反応した人の名前／アイコン／いいねかコメントか／対象の投稿
   const [data] = useState([
-    { name: "佐藤花子", action: "favo", img: "/usakus.jpg", tweet: "あああ" },
     {
       name: "佐藤花子",
-      action: "comment",
+      action: "お気に入り",
+      img: "/usakus.jpg",
+      tweet: "あああ",
+    },
+    {
+      name: "佐藤花子",
+      action: "コメント",
       img: "/usakus.jpg",
       tweet: "いいい",
     },
-    { name: "三角次郎", action: "favo", img: "/usakus.jpg", tweet: "あああ" },
+    {
+      name: "三角次郎",
+      action: "お気に入り",
+      img: "/usakus.jpg",
+      tweet: "あああ",
+    },
     {
       name: "山田太郎",
-      action: "comment",
+      action: "コメント",
       img: "/usakus.jpg",
       tweet: "うおお",
     },
     {
       name: "佐藤花子",
-      action: "favo",
+      action: "お気に入り",
       img: "/usakus.jpg",
       tweet: "良い気持ち",
     },
@@ -53,14 +63,25 @@ const Notion: NextPage = () => {
 
           {/* タイムラインゾーン */}
           {data.map((value, key) => (
-            <div style={style} key={key} className="flex">
+            <div style={style} key={key}>
               <div className="w-1/5 text-center pt-5">
+                {value.action === "お気に入り" && (
+                  <span className="text-2xl text-red-500">
+                    <i className="fas fa-heart"></i>
+                  </span>
+                )}
+                {value.action === "コメント" && (
+                  <span className="text-2xl text-yellow-600">
+                    <i className="fas fa-comment"></i>
+                  </span>
+                )}
+
                 <Image src={value.img} width={100} height={100} alt="icon" />
               </div>
 
               <div className="w-4/5">
-                <div className="text-xl font-extrabold pt-3 pb-3">
-                  {value.name}
+                <div className="text-xl pt-3 pb-3">
+                  {value.name}さんがあなたの投稿に{value.action}しました
                 </div>
                 <div className="pt-5 pb-5 pl-5 w-8/12">{value.tweet}</div>
               </div>
