f0a87af7a37d1032d1a508a60995362eedbf22c1
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-07 15:21:16 +0900

    TsDocコメント追加

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -5,7 +5,12 @@ export type Props = {
   longitude: string; //経度
 };
 
+/**
+ * Googleマップコンポーネント.
+ * @remarks 緯度と経度をstring型で渡してあげてください
+ */
 export const GoogleMap: FC<Props> = memo((props) => {
+  //初期設定のスタイルなので、CSSレストランページに合わせて修正して下さい
   const style = {
     frameborder: "0",
     scrolling: "no",
