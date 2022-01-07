aacf873ab675b472cac1e7111835174b483d8571
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-07 14:09:29 +0900

    通知ファイル作成

---
+++ src/pages/notion.tsx
@@ -0,0 +1,11 @@
+import { useState, useCallback } from "react";
+import { NextPage } from "next";
+
+/**
+ * 通知ページ.
+ * @returns 通知が見れるページ
+ */
+const Notion: NextPage = () => {
+  return <></>;
+};
+export default Notion;
