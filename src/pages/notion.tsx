608fb606988770b43db9b086bf8140b37032367f
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-07 14:23:37 +0900

    一旦タイムラインの画面をコピペしてきた／テストデータを整えた

--- src/pages/notion.tsx
+++ src/pages/notion.tsx
@@ -1,11 +1,74 @@
+import Image from "next/image";
 import { useState, useCallback } from "react";
 import { NextPage } from "next";
+import { SubHeader } from "../components/SubHeader";
+import { MenuBar } from "../components/MenuBar";
 
 /**
  * 通知ページ.
+ * @remarks ログインしているユーザのIDをAPIで送って、該当の情報を取得
  * @returns 通知が見れるページ
  */
 const Notion: NextPage = () => {
-  return <></>;
+  //テストデータ
+  //反応した人の名前／アイコン／いいねかコメントか／対象の投稿
+  const [data] = useState([
+    { name: "佐藤花子", action: "favo", img: "/usakus.jpg", tweet: "あああ" },
+    {
+      name: "佐藤花子",
+      action: "comment",
+      img: "/usakus.jpg",
+      tweet: "いいい",
+    },
+    { name: "三角次郎", action: "favo", img: "/usakus.jpg", tweet: "あああ" },
+    {
+      name: "山田太郎",
+      action: "comment",
+      img: "/usakus.jpg",
+      tweet: "うおお",
+    },
+    {
+      name: "佐藤花子",
+      action: "favo",
+      img: "/usakus.jpg",
+      tweet: "良い気持ち",
+    },
+  ]);
+
+  //1人1人のつぶやきの下に入る線がどうしてもtailwindで上手くいかなかった
+  const style = {
+    borderBottom: "solid 1px black",
+  };
+
+  return (
+    <>
+      <div className="flex">
+        <div>
+          <MenuBar />
+        </div>
+
+        {/* サブヘッダー */}
+        <div className="w-10/12">
+          <SubHeader title="通知" />
+
+          {/* タイムラインゾーン */}
+          {data.map((value, key) => (
+            <div style={style} key={key} className="flex">
+              <div className="w-1/5 text-center pt-5">
+                <Image src={value.img} width={100} height={100} alt="icon" />
+              </div>
+
+              <div className="w-4/5">
+                <div className="text-xl font-extrabold pt-3 pb-3">
+                  {value.name}
+                </div>
+                <div className="pt-5 pb-5 pl-5 w-8/12">{value.tweet}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 export default Notion;
