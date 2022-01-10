2a87d4873d1c7793305e30a487daeb84fecd4ab7
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-10 17:14:25 +0900

    変数の設定

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -1,5 +1,16 @@
-import { FC, memo } from "react";
+import { FC, memo, useState } from "react";
 
-export const Star: FC = memo(() => {
+export type Props = {
+  starCount: number;
+};
+
+export const Star: FC<Props> = memo((props) => {
+  //受け取った星の数
+  const { starCount } = props;
+  //星の数の少数切捨て
+  const [mathNum] = useState(Math.floor(starCount));
+  //星を入れる配列
+  const [starArray] = useState<string[]>([]);
+
   return <></>;
 });
