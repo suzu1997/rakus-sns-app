6dc61a00118a78e87bc8b0ea10c9be821f6f596e
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-10 22:54:31 +0900

    cookieをチェックするメソッド作成

--- src/components/LoginChecker.tsx
+++ src/components/LoginChecker.tsx
@@ -1,5 +1,15 @@
 import { memo, FC } from "react";
+import { useRouter } from "next/router";
+import Cookie from "universal-cookie";
 
 export const LoginChecker: FC = memo(() => {
+  //ルーターリンク
+  const router = useRouter();
+
+  const cookie = new Cookie();
+  if (cookie.get("id") === undefined) {
+    router.push("/login");
+  }
+
   return <>ログインチェッカー</>;
 });
