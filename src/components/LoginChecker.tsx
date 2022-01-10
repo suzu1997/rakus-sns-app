b2cce5c704d80981693b6c247b569f49e57d4fec
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-10 22:55:35 +0900

    TsDocコメント追加

--- src/components/LoginChecker.tsx
+++ src/components/LoginChecker.tsx
@@ -2,10 +2,16 @@ import { memo, FC } from "react";
 import { useRouter } from "next/router";
 import Cookie from "universal-cookie";
 
+/**
+ * ログインチェックコンポーネント
+ */
 export const LoginChecker: FC = memo(() => {
   //ルーターリンク
   const router = useRouter();
 
+  /**
+   * cookieをチェックしてログインページに飛ばす.
+   */
   const cookie = new Cookie();
   if (cookie.get("id") === undefined) {
     router.push("/login");
