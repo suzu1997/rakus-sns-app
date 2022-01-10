ef4dd4e1308e7e61c8cb58a63a29e58f4b1665cf
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-10 18:01:42 +0900

    投稿ボタンのCSS整え

--- src/components/PostBtn.tsx
+++ src/components/PostBtn.tsx
@@ -3,7 +3,10 @@ import { memo, FC } from "react";
 export const PostBtn: FC = memo(() => {
   return (
     <>
-      <button type="button">
+      <button
+        type="button"
+        className="rounded-full text-white bg-basic w-10 h-10"
+      >
         <i className="fas fa-plus"></i>
       </button>
     </>
