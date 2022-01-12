180da4a39c924d165db5ef6094eba66029587bfb
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-01-12 17:04:00 +0900

    サインアップのスペル修正２

--- src/pages/presignup.tsx
+++ src/pages/presignup.tsx
@@ -3,13 +3,13 @@ import { TextInput } from "../components/TextInput";
 import { Button } from "../components/Button";
 import { SelectBox } from "../components/SelectBox";
 import { NextPage } from "next";
-import { ConfModal } from "../components/ConfModal";
+import { useRouter } from "next/router";
 
 /**
  * ユーザー仮登録画面
  * @returns 仮登録するためのページ
  */
-const PreSingUp: NextPage = () => {
+const PreSignUp: NextPage = () => {
   //メールアドレスのドメイン選択肢
   const options = [
     { id: "1", name: "@rakus-patners.co.jp" },
@@ -24,9 +24,6 @@ const PreSingUp: NextPage = () => {
   //セレクトボックスの初期値
   const [selectValue, setSelectValue] = useState<string>(options[0].name);
 
-  // メール送信確認モーダルのオープン状態
-  const [isOpen, setIsOpen] = useState(false);
-
   //各入力フォームに入力した際に更新される
   const inputFirstNameValue = useCallback((e) => {
     setFirstName(e.target.value);
@@ -38,13 +35,19 @@ const PreSingUp: NextPage = () => {
     setEmail(e.target.value);
   }, []);
 
+  //ルーターリンク
+  const router = useRouter();
+
   //登録ボタンを押した時に呼ばれる
   const submitForm = () => {
     //バリデーションチェック後で実装、全てtrueならば以下に進む
 
     //本来はAPIにデータを送る
-    //メール送信が成功ならば、モーダル画面を開く
-    setIsOpen(true);
+    //仮登録が完了ならば、入力内容をクリアした後、仮登録完了画面に遷移する
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    router.push("/comppresignup");
   };
   //クリアボタンを押した時に呼ばれる
   const formClear = () => {
@@ -53,35 +56,13 @@ const PreSingUp: NextPage = () => {
     setEmail("");
   };
 
-  //モーダルを閉じるメソッド.
-  const closeModal = useCallback(() => {
-    setIsOpen(false);
-  }, []);
-
-  //モーダル中のボタンを押したときに呼ばれる
-  const doOnButton = useCallback(() => {
-    //入力内容をクリアする
-    setFirstName("");
-    setLastName("");
-    setEmail("");
-    //モーダルを閉じる
-    setIsOpen(false);
-  }, []);
-
   return (
     <>
       <div className="border-solid  border-2 border-bgc-200 m-10  shadow-lg rounded-xl text-center">
         <div className="text-3xl text-text-brown mt-5 font-bold ">
           仮登録フォーム
         </div>
-        <ConfModal
-          isOpen={isOpen}
-          closeModal={closeModal}
-          title="ご登録いただいたメールアドレス宛にメールを送信しました"
-          message="送信したメールより本登録をお願いします"
-          button="了解"
-          doOnButton={doOnButton}
-        />
+
         <div className="flex flex-col items-center mt-10">
           <div className="flex w-96 gap-3 mt-3">
             <TextInput
@@ -140,4 +121,4 @@ const PreSingUp: NextPage = () => {
     </>
   );
 };
-export default PreSingUp;
+export default PreSignUp;
