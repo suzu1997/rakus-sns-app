b1173557237012cfd045879dec3a3e03b5ff9fed
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-01-12 17:07:37 +0900

    ラジオボタンコンポーネント修正しました

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -3,14 +3,24 @@ import { FC } from "react";
 export type Props = {
   id: string;
   value: string;
+  name: string;
+  defaultChecked?: boolean;
 };
 
 export const Radio: FC<Props> = (props) => {
-  const { value, id } = props;
+  const { value, id, name, defaultChecked } = props;
   return (
     <div>
-      <input type="radio" name="sevice" value={value} id={id} />
-      <label htmlFor={id}>{id}</label>
+      <div className="flex-col text-left">
+        <input
+          type="radio"
+          name={name}
+          value={value}
+          id={id}
+          defaultChecked={defaultChecked}
+        />
+        <label htmlFor={id}>{id}</label>
+      </div>
     </div>
   );
 };
