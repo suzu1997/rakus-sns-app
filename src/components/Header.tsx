f38c26d08134bb84db83e89fd9a6588a77a0b666
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-13 13:14:16 +0900

    開閉用演算子追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { memo, FC, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { MenuBar } from "./MenuBar";
 
 export const Header: FC = memo(() => {
   //ヘッダー下線
@@ -42,40 +43,46 @@ export const Header: FC = memo(() => {
         </span>
       </header>
       <div className="relative">
-        <aside className="absolute top-0 right-0 bg-sidebar h-screen w-64 shadow-xl">
-          <nav className="text-white text-base font-semibold bg-basic pt-3">
-            <Link href="/aaa/test">
-              <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                HOME
-              </a>
-            </Link>
-            <Link href="/">
-              <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                プロフィール
-              </a>
-            </Link>
-            <Link href="/notion">
-              <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                通知
-              </a>
-            </Link>
-            <Link href="/learcen/info">
-              <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                ラーセン内情報
-              </a>
-            </Link>
-            <Link href="/lunch/review">
-              <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                ランチ情報
-              </a>
-            </Link>
-            <Link href="/logout">
-              <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                ログアウト
-              </a>
-            </Link>
-          </nav>
-        </aside>
+        {isOpen ? (
+          <div className="absolute top-0 right-0">
+            <aside className="relative bg-bgc h-screen w-64">
+              <nav className="text-white text-base font-semibold bg-basic pt-3">
+                <Link href="/aaa/test">
+                  <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                    HOME
+                  </a>
+                </Link>
+                <Link href="/">
+                  <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                    プロフィール
+                  </a>
+                </Link>
+                <Link href="/notion">
+                  <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                    通知
+                  </a>
+                </Link>
+                <Link href="/learcen/info">
+                  <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                    ラーセン内情報
+                  </a>
+                </Link>
+                <Link href="/lunch/review">
+                  <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                    ランチ情報
+                  </a>
+                </Link>
+                <Link href="/logout">
+                  <a className="flex items-center opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                    ログアウト
+                  </a>
+                </Link>
+              </nav>
+            </aside>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
