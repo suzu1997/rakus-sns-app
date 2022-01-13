0221421b808aebcdedb4f153699b202839315438
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-13 10:35:08 +0900

    APIRouteでホットペッパーAPIを叩く簡易APIを構築

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,1 +1,2 @@
 export const HOTPEPPER_URL = `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.NEXT_PUBLIC_API_KEY}&format=jsonp`;
+export const HOTPEPPER_URL2 = `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&format=json&lat=35.689445&lng=139.70735&range=3&count=50`;
