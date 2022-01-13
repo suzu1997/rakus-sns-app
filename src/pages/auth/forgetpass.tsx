fca68865c37edb2a236348bfba7a4c9c076bd269
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-01-13 13:41:08 +0900

    テキストフォームとボタンを追加

--- src/pages/auth/forgetpass.tsx
+++ src/pages/auth/forgetpass.tsx
@@ -1,4 +1,6 @@
 import { NextPage } from "next";
+import { TextInput } from "../../components/TextInput";
+import { Button } from "../../components/Button";
 /**
  * パスワードを忘れたときの画面
  * @returns パスワードを忘れたときの画面
@@ -12,6 +14,28 @@ const ForgetPass: NextPage = () => {
         <div className="mt-10 ">
           ご登録いただいているメールアドレスをご入力してください
         </div>
+        <form name="SignupForm" noValidate>
+          <div className="flex flex-col items-center mt-5">
+            <div className="gap-3 w-96 mt-3">
+              {/* メールアドレスのテキストフォーム */}
+              <TextInput
+                label="メールアドレス"
+                type="text"
+                fullWidth={true}
+                required
+                placeholder="メールアドレス"
+              />
+            </div>
+            <div className="mt-10 mb-10">
+              <Button
+                label="送信"
+                backgroundColor="#f28728"
+                color="white"
+                size="md"
+              />
+            </div>{" "}
+          </div>{" "}
+        </form>
       </div>
     </>
   );
