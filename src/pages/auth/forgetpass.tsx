5592ef8d3aee03ef521dffd64fdea1068fc43f46
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-01-13 13:29:29 +0900

    パスワード忘れたとき画面新規作成

---
+++ src/pages/auth/forgetpass.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from "next";
+/**
+ * パスワードを忘れたときの画面
+ * @returns パスワードを忘れたときの画面
+ */
+const ForgetPass: NextPage = () => {
+  };
+
+  return (
+    <>
+      <div className="border-solid  border-2 border-bgc-200 m-10  shadow-lg rounded-xl text-center">
+        <div className="mt-10 ">
+          ご登録いただいているメールアドレスをご入力してください
+        </div>
+      </div>
+    </>
+  );
+};
+export default ForgetPass;
