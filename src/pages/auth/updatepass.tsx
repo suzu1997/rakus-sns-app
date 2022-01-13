e98143419079b50504a1a3f6eda9c9b9332591a4
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-01-13 13:51:29 +0900

    テキストフォームとボタンを追加

--- src/pages/auth/updatepass.tsx
+++ src/pages/auth/updatepass.tsx
@@ -1,4 +1,6 @@
 import { NextPage } from "next";
+import { TextInput } from "../../components/TextInput";
+import { Button } from "../../components/Button";
 /**
  * パスワードを忘れたときの画面
  * @returns パスワードを忘れたときの画面
@@ -10,6 +12,48 @@ const UpdatePass: NextPage = () => {
         <div className="mt-10 ">
           以下のフォームよりパスワードの更新をお願いします
         </div>
+        <form name="SignupForm" noValidate>
+          <div className="flex flex-col items-center mt-5">
+            <div className="gap-3 w-96 mt-3">
+              {/* メールアドレスのテキストフォーム */}
+              <TextInput
+                label="メールアドレス"
+                type="text"
+                fullWidth={true}
+                required
+                placeholder="メールアドレス"
+              />
+            </div>
+            <div className="w-96 mt-3">
+              {/* パスワードのテキストフォーム */}
+              <TextInput
+                label="パスワード(半角英数字)"
+                type="password"
+                fullWidth={true}
+                required
+                placeholder="8文字以上16文字以内(大文字小文字数字含む)"
+              />
+            </div>
+            <div className="w-96 mt-3">
+              {/* 確認用パスワードのテキストフォーム */}
+              <TextInput
+                label="確認用パスワード(半角英数字)"
+                type="password"
+                fullWidth={true}
+                required
+                placeholder="8文字以上16文字以内(大文字小文字数字含む)"
+              />
+            </div>
+            <div className="mt-10 mb-10">
+              <Button
+                label="変更"
+                backgroundColor="#f28728"
+                color="white"
+                size="md"
+              />
+            </div>{" "}
+          </div>{" "}
+        </form>
       </div>
     </>
   );
