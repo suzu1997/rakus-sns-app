62ce1147e6cd68d2d418a5051543817aa5fe19c9
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-18 17:35:39 +0900

    店情報の方を定義

---
+++ src/types/type.ts
@@ -0,0 +1,11 @@
+export type Restaurant = {
+  restaurantId: number;
+  restaurantName: string;
+  restaurantAddress: string;
+  restaurantGenre: string;
+  restaurantStar: number;
+  restaurantType: number;
+  restaurantImg: string;
+  updatedTime: Date;
+  postedTime: Date;
+}
