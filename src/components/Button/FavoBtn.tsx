764814c584784edfb46d3108f057dc0d2eec69b6
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-18 15:53:44 +0900

    いいねボタンのON/OFF設定

--- src/components/Button/FavoBtn.tsx
+++ src/components/Button/FavoBtn.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useCallback } from "react";
+import { FC, memo, useCallback, useState } from "react";
 
 type Props = {
   postId?: number;
@@ -11,30 +11,42 @@ export const FavoBtn: FC<Props> = memo((props) => {
   //お気に入り(いいね)対象の投稿番号
   const { postId = 0 } = props;
 
+  const [isFavo, setIsFavo] = useState(false);
+
   /**
    * はいボタン押下で発動.
    */
   const favo = useCallback(async () => {
-    alert("投稿ID" + postId + "をいいねしました");
-    // try {
-    //   const res = await axios.post("http://localhost:8080/signup");
-    //   console.log(JSON.stringify(res.data));
-    //   if (res.data.status === "success") {
-    //     console.log(res.data.status);
-    //     //成功→タイムラインページに戻る
-    //     closeModal();
-    //   } else {
-    //     alert(res.data.message);
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    // }
-  }, []);
+    if (!isFavo) {
+      alert("投稿ID" + postId + "をいいねしました");
+      setIsFavo(true);
+      // try {
+      //   const res = await axios.post("http://localhost:8080/signup");
+      //   console.log(JSON.stringify(res.data));
+      //   if (res.data.status === "success") {
+      //     console.log(res.data.status);
+      //     //成功→タイムラインページに戻る
+      //     closeModal();
+      //   } else {
+      //     alert(res.data.message);
+      //   }
+      // } catch (error) {
+      //   console.log(error);
+      // }
+    } else {
+      setIsFavo(false);
+    }
+    //[]内入れないと変更が反映しないため挿入
+  }, [isFavo, postId]);
 
   return (
     <>
       <button type="button" className="pr-10" onClick={favo}>
-        <i className="fas fa-heart text-gray-500 hover:text-red-500"></i>
+        {isFavo ? (
+          <i className="fas fa-heart text-red-500"></i>
+        ) : (
+          <i className="fas fa-heart text-gray-500 hover:text-red-500"></i>
+        )}
       </button>
     </>
   );
