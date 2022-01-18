bee8ddd7352a669d7d6d352a56e0aedab678b12f
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-01-18 10:40:50 +0900

    axiosインポート

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -7,6 +7,7 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import { TextInput } from "../../components/Form/TextInput";
+import axios from "axios";
 
 //バリデーションチェック
 const schema = yup.object().shape({
