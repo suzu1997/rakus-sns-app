9563e3f0cb0cd7035aa9b0844dadb42c09db0986
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-18 10:59:27 +0900

    ホットペッパーURL追加

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,12 +1,20 @@
+import { memo, FC } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
 /**
  * footer.
  */
-import { memo, FC } from "react";
-
 export const Footer: FC = memo(() => {
   return (
     <footer className="h-12 w-full shadow-md bg-basic text-sm text-gray-600 flex justify-center items-center">
-      ©lunchkus
+      <div>©lunchkus</div>
+      <div>
+        Powered by
+        <Link href="http://webservice.recruit.co.jp/">
+          <a>ホットペッパー Webサービス</a>
+        </Link>
+      </div>
     </footer>
   );
 });
