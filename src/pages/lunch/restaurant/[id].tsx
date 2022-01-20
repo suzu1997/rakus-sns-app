db25d778942b8c0fb3b128b21bbb559bdc05ea1d
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-20 18:16:33 +0900

    お店詳細情報をAPIから取得するようにした

--- src/pages/lunch/restaurant/[id].tsx
+++ src/pages/lunch/restaurant/[id].tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useState, useCallback } from "react";
+import useSWR from "swr";
 import { Button } from "../../../components/Button/Button";
 import { GoogleMap } from "../../../components/Lunch/GoogleMap";
 import { ReviewList } from "../../../components/Lunch/ReviewList";
@@ -8,6 +9,8 @@ import { Star } from "../../../components/Lunch/Star";
 import { PostModal } from "../../../components/Modal/PostModal";
 import Image from "next/image";
 import { SubHeader } from "../../../components/Layout/SubHeader";
+import { JAVA_API_URL } from "../../../utils/const";
+import { Restaurant } from "../../../types/type";
 
 /**
  * お店情報の詳細を表示するページ.
@@ -18,6 +21,11 @@ const RestaurantDetail: NextPage = () => {
   // レビュー投稿のモーダルのオープン状態
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
+
+  // idをURLから取得
+  const restaurantId = Number(router.query.id);
+
+  const { data, error } = useSWR(`${JAVA_API_URL}/restaurant/${restaurantId}`);
 
   /**
    * モーダルを閉じるメソッド.
@@ -33,21 +41,33 @@ const RestaurantDetail: NextPage = () => {
     setIsOpen(true);
   }, [setIsOpen]);
 
-  // ダミーデータ
-  const dummyData = {
-    id: "J001041443",
-    name: "らーめん 氣華",
-    genre: "ラーメン",
-    type: "店内",
-    star: 4.5,
-    img: "https://imgfp.hotp.jp/IMGH/76/15/P018367615/P018367615_238.jpg",
-    address: "東京都新宿区新宿２－７－５松田ビル１F",
-    access:
-      "新宿三丁目駅から新宿通りを新宿御苑前駅方面に進んでいただいて、徒歩4分大通りに面しております。",
-    lat: "35.6896771976", // 緯度
-    lng: "139.7079539006", // 経度
-    url: "https://www.hotpepper.jp/strJ001041443/?vos=nhppalsa000016",
-    catch: "野菜を使ったメニュー サイドメニューもご用意",
+  if (!error && !data) {
+    return (
+      <div className="flex justify-center pt-10 w-full">
+        <div className="animate-spin h-8 w-8 bg-basic rounded-xl"></div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>データを取得できませんでした</div>;
+  }
+
+  // レストラン情報をdataから抽出
+  const restaurant: Restaurant = data.restaurant;
+
+  /**
+   * タイプのidから文字列に変換する.
+   * @returns 店内かお弁当か両方か
+   */
+  const typeValue = () => {
+    if (restaurant.type === 1) {
+      return "店内";
+    } else if (restaurant.type === 2) {
+      return "お弁当";
+    } else if (restaurant.type === 3) {
+      return "両方";
+    }
   };
 
   return (
@@ -65,38 +85,38 @@ const RestaurantDetail: NextPage = () => {
               ←戻る
             </div>
             <p className="text-3xl font-extrabold border-l-8 border-basic mb-5">
-              {dummyData.name}
+              {restaurant.name}
             </p>
             <div className="flex flex-col sm:flex-row items-baseline">
               <span className="mr-8 mb-5 sm:mb-0">
-                <Star starCount={dummyData.star} />
+                <Star starCount={restaurant.star} />
               </span>
-              ジャンル: {dummyData.genre}
-              <span className="sm:ml-8">タイプ: {dummyData.type}</span>
+              ジャンル: {restaurant.genreValue}
+              <span className="sm:ml-8">タイプ: {typeValue()}</span>
             </div>
             <div className="mt-5 sm:mt-10">
               <div>
                 <Image
-                  src={dummyData.img}
+                  src={`/${restaurant.photoPath}`}
                   width={300}
                   height={200}
                   alt="restaurant photo"
                 />
               </div>
-              <div className="mt-10">{dummyData.catch}</div>
+              <div className="mt-10">{restaurant.description}</div>
             </div>
-            <p className="mt-10">住所: {dummyData.address}</p>
-            <p className="mt-10">アクセス: {dummyData.access}</p>
+            <p className="mt-10">住所: {restaurant.address}</p>
+            <p className="mt-10">アクセス: {restaurant.access}</p>
             {/* 緯度と軽度から、googleマップを表示 */}
             <div className="w-5/6 mx-auto">
               <p className="mt-10">Map</p>
-              <GoogleMap latitude={dummyData.lat} longitude={dummyData.lng} />
+              <GoogleMap
+                latitude={restaurant.latitude}
+                longitude={restaurant.longitude}
+              />
             </div>
             <p className="mt-10">
-              ホットペッパーURL:{" "}
-              <a href="https://www.hotpepper.jp/strJ001041443/?vos=nhppalsa000016">
-                https://www.hotpepper.jp/strJ001041443/?vos=nhppalsa000016
-              </a>
+              ホットペッパーURL: <a href={restaurant.url}>{restaurant.url}</a>
             </p>
           </div>
           <div className="lg:w-1/3 mt-10 ml-auto">
@@ -113,7 +133,7 @@ const RestaurantDetail: NextPage = () => {
           isOpen={isOpen}
           closeModal={closeModal}
           title={"レビュー"}
-          restaurantId={dummyData.id}
+          restaurantId={restaurant.id}
         />
       </div>
     </div>
