d6c2e07297e90a3d5305fe909b4c0091f7720a79
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-21 11:48:08 +0900

    緯度経度がばがばAPIは出来た

--- src/pages/aaa/test.tsx
+++ src/pages/aaa/test.tsx
@@ -6,26 +6,30 @@ import { TextInput } from "../../components/Form/TextInput";
 import Cookie from "universal-cookie";
 import axios from "axios";
 import { Button } from "../../components/Button/Button";
+import { GoogleMap } from "../../components/Lunch/GoogleMap";
 
 const Test: NextPage = () => {
-  const [idoTest, setIdoTest] = useState();
-  const [keidoTest, setKeidoTest] = useState();
+  const [latitudeData, setLatitudeData] = useState("");
+  const [longitudeData, setLongitudeData] = useState("");
 
   /**
    * 緯度経度API
    */
-  const latitudeLongitude = async () => {
+  const latitudeLongitude = useCallback(async () => {
     const url = "https://msearch.gsi.go.jp/address-search/AddressSearch?q=";
     //この変数に住所が入るようにする
-    const address = "北海道札幌市北区北６条西４丁目";
+    // const address = "北海道札幌市北区北６条西４丁目";
+    const address = "東京都中央区八重洲１丁目４丁目２２番地";
     const res = await axios.get(`${url}${address}`);
     //緯度
-    console.dir("緯度" + JSON.stringify(res.data[0].geometry.coordinates[0]));
-    setIdoTest(res.data[0].geometry.coordinates[0]);
+    const latitude = String(res.data[0].geometry.coordinates[1]);
+    console.dir("緯度" + latitude + "0");
+    setLatitudeData(latitude + "0");
     //経度
-    console.dir("経度" + JSON.stringify(res.data[0].geometry.coordinates[1]));
-    setKeidoTest(res.data[0].geometry.coordinates[1]);
-  };
+    const longitude = String(res.data[0].geometry.coordinates[0]);
+    setLongitudeData(longitude + "0");
+    console.dir("経度" + longitude + "0");
+  }, []);
 
   const options = [
     { id: "1", name: "Wade Cooper" },
@@ -49,6 +53,10 @@ const Test: NextPage = () => {
   return (
     <div className="flex flex-col items-center mt-10">
       <Button label="緯度経度" onClick={latitudeLongitude} />
+      {latitudeData != "" && longitudeData != "" && (
+        <GoogleMap latitude={latitudeData} longitude={longitudeData} />
+      )}
+      <GoogleMap latitude="35.6816166" longitude="139.7676585" />
 
       <p className="font-mono text-red-700">テスト</p>
       <Link href="/">
