eb92e39188dad680122ab0a48dfa5d0179a776ec
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-23 15:04:25 +0900

    検索エリアの文言変更&クリアボタン設置

--- src/pages/lunch/restaurant/search.tsx
+++ src/pages/lunch/restaurant/search.tsx
@@ -110,6 +110,16 @@ const RestaurantSearch: FC = () => {
     [router],
   );
 
+  /**
+   * 検索結果を初期表示にリセットする.
+   */
+  const clear = () => {
+    setRestaurantsInDB([]);
+    setHotpeppers([]);
+    setHasClickedSearch(false);
+    setSearchName("");
+  };
+
   return (
     <div className="flex">
       <div className="flex-1">
@@ -130,7 +140,9 @@ const RestaurantSearch: FC = () => {
           {/* データベースに登録済みの店をオートコンプリートに表示する部分 */}
           {restautrantsInDB.length > 0 && (
             <>
-              <p className="mb-5">もしかしてこのお店？(登録済みのお店)</p>
+              <p className="mb-5 font-bold">
+                もしかしてこのお店？(登録済みのお店)
+              </p>
               <ul>
                 {restautrantsInDB.map((restautrant) => {
                   return (
@@ -167,37 +179,55 @@ const RestaurantSearch: FC = () => {
           {/* ホットペッパー検索結果表示 */}
           {hasClickedSearch &&
             (hotpeppers.length > 0 ? (
-              <ul>
-                {hotpeppers.map((hotpepper) => {
-                  return (
-                    <li key={hotpepper.id} className="flex items-center mb-3">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-3 w-3 mr-3"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span
-                        className="cursor-pointer hover:text-text-brown hover:underline"
-                        onClick={() => selectRestaurant(hotpepper)}
-                      >
-                        {hotpepper.name}
-                      </span>
-                    </li>
-                  );
-                })}
-              </ul>
+              <>
+                <p className="my-5 font-bold">検索結果(Hotppepperより検索)</p>
+                <ul>
+                  {hotpeppers.map((hotpepper) => {
+                    return (
+                      <li key={hotpepper.id} className="flex items-center mb-3">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-3 w-3 mr-3"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                        <span
+                          className="cursor-pointer hover:text-text-brown hover:underline"
+                          onClick={() => selectRestaurant(hotpepper)}
+                        >
+                          {hotpepper.name}
+                        </span>
+                      </li>
+                    );
+                  })}
+                </ul>
+                <div className="my-5 font-bold">
+                  お目当てのお店が見つかりませんか？手入力でお店を登録しますか？
+                </div>
+                <div className="flex gap-2">
+                  <Button
+                    label="手入力で登録"
+                    onClick={() => router.push("/lunch/restaurant/add")}
+                  />
+                  <Button
+                    label="クリア"
+                    onClick={clear}
+                    backgroundColor="#f6f0ea"
+                    color="#622d18"
+                  />
+                </div>
+              </>
             ) : (
               <>
-                <div className="mb-3">
+                <div className="mb-3 font-bold">
                   検索結果が見つかりませんでした。手入力でお店を登録しますか？
                 </div>
                 <div className="flex gap-2">
