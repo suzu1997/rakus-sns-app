dd3d121bcdedd226b7308edae608376179761154
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-23 19:10:40 +0900

    hooksのファイル名変更と修正

--- src/hooks/useGetAddress.ts
+++ src/hooks/useGetAddress.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import { FieldValues, UseFormSetValue } from "react-hook-form";
 
 /**
  * 郵便番号から住所を取得できるhook.
@@ -7,20 +8,18 @@ import { useState } from "react";
  * @returns 取得メソッド, 住所, エラー
  */
 export const useGetAddress = (zipcode: string) => {
-  //住所
-  const [address, setAddress] = useState("");
   //エラー(空欄時／検索出来なかった時表示)
   const [errorOfAddress, setErrorOfAddress] = useState("");
   /**
    * 郵便番号から住所を取得.
+   * @param setValue - React-hook-formのsetValueメソッド。
+   * inputの値を動的に設定するために必要。
    */
-  const getAddress = async () => {
-    //初期値リセット(住所、住所エラー)
-    setAddress("");
+  const getAddress = async (setValue: UseFormSetValue<FieldValues>) => {
+    //初期値リセット(住所エラー)
     setErrorOfAddress("");
     //郵便番号から住所を取得APIに郵便番号を送る
     try {
-      // const axios = require("axios");
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       const axiosJsonpAdapter = require("axios-jsonp");
 
@@ -34,7 +33,9 @@ export const useGetAddress = (zipcode: string) => {
       if (response.data.length === 1) {
         const data =
           response.data.items[0].state_name + response.data.items[0].address;
-        setAddress(String(data));
+        // react-hook-formのsetValueメソッドを使ってinputの値を動的に設定
+        setValue("address", String(data));
+
         //失敗したらエラーを出す
       } else {
         setErrorOfAddress("住所が見つかりません");
@@ -43,5 +44,5 @@ export const useGetAddress = (zipcode: string) => {
       setErrorOfAddress("住所が見つかりません");
     }
   };
-  return { getAddress, address, errorOfAddress };
+  return { getAddress, errorOfAddress };
 };
