89adcbe8b84358089aba61e252d385c9a9c0d93b
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-25 02:11:45 +0900

    レビュー投稿後、店詳細ページ(店情報(星), レビュー)が更新されるようにした

--- src/pages/lunch/restaurant/[id].tsx
+++ src/pages/lunch/restaurant/[id].tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useContext } from "react";
 import { Button } from "../../../components/Button/Button";
 import { ReviewList } from "../../../components/Lunch/ReviewList";
 import { PostModal } from "../../../components/Modal/PostModal";
@@ -9,6 +9,8 @@ import { RestaurantDetailContainer } from "../../../components/Lunch/RestaurantD
 import useSWR from "swr";
 import { JAVA_API_URL } from "../../../utils/const";
 import { Restaurant } from "../../../types/type";
+import { loginIdContext } from "../../../providers/LoginIdProvider";
+import { useSWRReviews } from "../../../hooks/useSWRReviews";
 
 /**
  * お店情報の詳細を表示するページ.
@@ -19,6 +21,11 @@ const RestaurantDetail: NextPage = () => {
   // レビュー投稿のモーダルのオープン状態
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
+
+  const userId = useContext(loginIdContext);
+
+  // レビュー一覧を再検証・再取得する関数をhooksから取得
+  const { reviewsMutate } = useSWRReviews(userId);
 
   /**
    * モーダルを閉じるメソッド.
@@ -37,7 +44,20 @@ const RestaurantDetail: NextPage = () => {
   // idをURLから取得
   const restaurantId = Number(router.query.id);
 
-  const { data, error } = useSWR(`${JAVA_API_URL}/restaurant/${restaurantId}`);
+  const { data, error, mutate } = useSWR(
+    `${JAVA_API_URL}/restaurant/${restaurantId}`,
+  );
+
+  /**
+   * 店詳細の情報を更新するメソッド.
+   *
+   * @remarks
+   * レビュー投稿が成功すると呼ばれる。
+   */
+  const updateData = useCallback(() => {
+    reviewsMutate(); // レビュー一覧を再検証・再取得する
+    mutate(); // レストラン情報を再検証・再取得する
+  }, [mutate, reviewsMutate]);
 
   if (!error && !data) {
     return (
@@ -84,6 +104,7 @@ const RestaurantDetail: NextPage = () => {
               closeModal={closeModal}
               title={"レビュー"}
               restaurantId={restaurantId}
+              success={updateData}
             />
           </div>
         </div>
