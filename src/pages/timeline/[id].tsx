1cb843e6d153c585dd84d33ec336f40df43f7765
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-24 13:29:18 +0900

    データなかった場合非表示に変更

--- src/pages/timeline/[id].tsx
+++ src/pages/timeline/[id].tsx
@@ -49,7 +49,6 @@ const TweetDetail: NextPage = () => {
    * APIを使用してタイムラインデータを取得.(未実装)
    */
   const { data, error } = useSWR(`${JAVA_API_URL}/timeline/detail/${postId}`);
-  console.dir(JSON.stringify(data.timeline));
 
   //つぶやき詳細データ
   const [detailData] = useState<TimelineDtail>(data?.timeline);
@@ -74,75 +73,89 @@ const TweetDetail: NextPage = () => {
       </div>
 
       {/* つぶやき詳細 */}
-      <div className="mx-5 mt-10">
-        <div>
-          <div className="px-3 flex">
-            <div className="w-3/12 cursor-pointer hover:opacity-50">
-              <Image
-                src={`/image/userIcon/${detailData.userPhotoPath}`}
-                width={200}
-                height={200}
-                alt="icon"
-                onClick={() => {
-                  goUserPage(detailData.userId);
-                }}
-                className="rounded-full"
-              />
-            </div>
-            <div className="w-9/12">
-              <div className="text-xl font-extrabold py-3 ml-3">
-                {data.name}
+      {detailData && (
+        <div className="mx-5 mt-10">
+          <div>
+            <div className="px-3 flex">
+              <div className="w-3/12 cursor-pointer hover:opacity-50">
+                <Image
+                  src={`/image/userIcon/${detailData.userPhotoPath}`}
+                  width={200}
+                  height={200}
+                  alt="icon"
+                  onClick={() => {
+                    goUserPage(detailData.userId);
+                  }}
+                  className="rounded-full"
+                />
               </div>
-              <div className="w-8/12 ml-5">{detailData.sentence}</div>
+              <div className="w-9/12">
+                <div className="text-xl font-extrabold py-3 ml-3">
+                  {data.name}
+                </div>
+                <div className="w-8/12 ml-5">{detailData.sentence}</div>
+              </div>
+            </div>
+
+            <div className="text-right pb-5" style={style}>
+              <div className="flex flex-col items-end gap-3 sm:flex-row justify-end mr-5 mt-5">
+                <div className="mr-5">投稿日時：{detailData.postedTime}</div>
+                <div>
+                  <CommentIcon
+                    commentCount={detailData.commentCount}
+                    postId={detailData.id}
+                    target="timeline"
+                  />
+                  <FavoBtn
+                    postId={data.postId}
+                    favoCount={detailData.likeCount}
+                  />
+                  {loginId == data.userId && (
+                    <TrashBtn postId={detailData.id} />
+                  )}
+                </div>
+              </div>
             </div>
           </div>
 
-          <div className="text-right pb-5" style={style}>
-            <div className="flex flex-col items-end gap-3 sm:flex-row justify-end mr-5 mt-5">
-              <div className="mr-5">投稿日時：{detailData.postedTime}</div>
-              <div>
-                <CommentIcon
-                  commentCount={detailData.commentCount}
-                  postId={detailData.id}
-                  target="timeline"
-                />
-                <FavoBtn
-                  postId={data.postId}
-                  favoCount={detailData.likeCount}
-                />
-                {loginId == data.userId && <TrashBtn postId={detailData.id} />}
-              </div>
+          {commentList ? (
+            <div>
+              {commentList.map((value, key) => (
+                <div style={style} key={key} className="flex">
+                  <div className="w-1/5 text-center pt-5 cursor-pointer hover:opacity-50">
+                    <Image
+                      src={`/image/userIcon/${value.userPhotoPath}`}
+                      width={100}
+                      height={100}
+                      alt="icon"
+                      onClick={() => {
+                        goUserPage(value.userId);
+                      }}
+                      className="rounded-full"
+                    />
+                  </div>
+                  <div className="w-4/5">
+                    <div className="text-xl font-extrabold py-3 ml-3">
+                      {value.accountName}
+                    </div>
+                    <div className="pt-5 pb-5 pl-5 w-8/12">
+                      {value.sentence}
+                    </div>
+                    <div className="w-full text-right py-3 pr-5">
+                      <FavoBtn postId={value.id} favoCount={value.likeCount} />
+                      {loginId == value.userId && (
+                        <TrashBtn postId={value.id} />
+                      )}
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
-          </div>
+          ) : (
+            <div>コメントはありません</div>
+          )}
         </div>
-        {commentList.map((value, key) => (
-          <div style={style} key={key} className="flex">
-            <div className="w-1/5 text-center pt-5 cursor-pointer hover:opacity-50">
-              <Image
-                src={`/image/userIcon/${value.userPhotoPath}`}
-                width={100}
-                height={100}
-                alt="icon"
-                onClick={() => {
-                  goUserPage(value.userId);
-                }}
-                className="rounded-full"
-              />
-            </div>
-            <div className="w-4/5">
-              <div className="text-xl font-extrabold py-3 ml-3">
-                {value.accountName}
-              </div>
-              <div className="pt-5 pb-5 pl-5 w-8/12">{value.sentence}</div>
-              <div className="w-full text-right py-3 pr-5">
-                <FavoBtn postId={value.id} favoCount={value.likeCount} />
-                {loginId == value.userId && <TrashBtn postId={value.id} />}
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-
+      )}
       <div>
         <PostBtn />
       </div>
