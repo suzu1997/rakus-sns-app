3ffbbca8cbcfe0db989dd7317006d3fa928b20e1
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-24 11:00:56 +0900

    テスト用ホットペッパーAPIのURLを削除

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,5 @@
 
-export const HOTPEPPER_URL = `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.NEXT_PUBLIC_API_KEY}&format=jsonp`;
-export const HOTPEPPER_URL2 = `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&format=json&lat=35.689445&lng=139.70735&range=3&count=50`;
+export const HOTPEPPER_URL = `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&format=json&lat=35.689445&lng=139.70735&range=3&count=50`;
 
 // JavaのAPIを叩くためのURLの共通部分
 export const JAVA_API_URL = "http://localhost:8080";
