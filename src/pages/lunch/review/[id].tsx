25cfaac72e274696c96f8573089751816b208e09
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-24 20:03:00 +0900

    レビュー詳細をAPIから取得できるように

--- src/pages/lunch/review/[id].tsx
+++ src/pages/lunch/review/[id].tsx
@@ -11,25 +11,25 @@ import { LunchReview } from "../../../types/type";
 import { useContext } from "react";
 import { loginIdContext } from "../../../providers/LoginIdProvider";
 
-type ReviewWithComment = LunchReview & {
-  comment?: { value: string; count: number }[];
-};
-
 const ReviewDetail: NextPage = () => {
   const router = useRouter();
   const reviewId = Number(router.query.id);
 
   const userId = useContext(loginIdContext);
 
-  const { data: review, error } = useSWR<ReviewWithComment>(`${JAVA_API_URL}/review/${reviewId}/${userId}`);
+  const { data, error } = useSWR(
+    `${JAVA_API_URL}/review/detail/${reviewId}/${userId}`,
+  );
 
-  if (!error && !review) {
+  if (!error && !data) {
     return <div>Loading...</div>;
   }
 
   if (error) {
     return <div>データを取得できませんでした</div>;
   }
+
+  const review: LunchReview = data.review;
 
   return (
     <div className="flex">
@@ -47,7 +47,7 @@ const ReviewDetail: NextPage = () => {
           <>
             <ReviewCard {...review} type="詳細" hasRestaurantInfo={true} />
             {/* コメント部分 */}
-            {review.comment?.map((value: any, index: number) => (
+            {data.commentList.map((value: any, index: number) => (
               <div key={index} className="flex border border-b border-gray-200">
                 <div className="w-1/5 text-center pt-5">
                   <Image
