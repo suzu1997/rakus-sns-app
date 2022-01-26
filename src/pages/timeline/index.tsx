b344248774cd64522df2f5c697075c6d9318aae3
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-26 11:43:17 +0900

    タイムラインの無限ローディングやってみた

--- src/pages/timeline/index.tsx
+++ src/pages/timeline/index.tsx
@@ -29,7 +29,13 @@ const Timeline: NextPage = () => {
   const router = useRouter();
 
   // 投稿一覧を再検証・再取得する関数をhooksから取得
-  const { timelineMutate } = useSWRTimeline(loginId);
+  const {
+    data,
+
+    error,
+    loadMoreTimeline,
+    timelineMutate,
+  } = useSWRTimeline(loginId);
 
   /**
    * 画像クリックで投稿ユーザ情報ページに飛ぶ.
@@ -50,9 +56,9 @@ const Timeline: NextPage = () => {
   /**
    * APIを使用してタイムラインデータを取得.
    */
-  const { data, error, mutate } = useSWR(`${JAVA_API_URL}/timeline/${loginId}`);
-  // タイムライン情報をdataから抽出
-  const timelineData: Timeline = data?.TimelineList;
+  // const { data, error, mutate } = useSWR(`${JAVA_API_URL}/timeline/${loginId}`);
+  // // タイムライン情報をdataから抽出
+  // const timelineData: Timeline = data?.TimelineList;
 
   /**
    * タイムラインの情報を更新するメソッド.
@@ -62,38 +68,38 @@ const Timeline: NextPage = () => {
    */
   const updateData = useCallback(() => {
     timelineMutate(); // タイムライン一覧を再検証・再取得する
-    mutate(); // タイムライン情報を再検証・再取得する
-  }, [mutate, timelineMutate]);
+    // mutate(); // タイムライン情報を再検証・再取得する
+  }, [timelineMutate]);
 
   /**
    * 古い投稿の読み込み直し.
    */
-  const getOldData = useCallback(async () => {
-    //データの最後のidを取得
-    const oldNumber = timelineData?.length - 1;
-    const oldId = timelineData?.[oldNumber].id;
-    try {
-      const res = await axios.get(
-        `${JAVA_API_URL}/timeline/old/${oldId}/${loginId}`,
-      );
-      const oldList: Timeline = res.data.TimelineList;
-      //取得した古いリストが空だったらreturn(lengthが上手く取れないのでこのような処理)
-      if (!oldList[0]) {
-        toast.success("過去の投稿はありません");
-        return;
-      }
-      //空でなければ既存のリストに追加
-      for (const oldData of oldList) {
-        timelineData.push(oldData);
-      }
-      toast.success("過去の投稿を読み込みました");
-    } catch (error) {
-      console.log(error);
-    }
-  }, [loginId, timelineData]);
+  // const getOldData = useCallback(async () => {
+  //   //データの最後のidを取得
+  //   const oldNumber = timelineData?.length - 1;
+  //   const oldId = timelineData?.[oldNumber].id;
+  //   try {
+  //     const res = await axios.get(
+  //       `${JAVA_API_URL}/timeline/old/${oldId}/${loginId}`,
+  //     );
+  //     const oldList: Timeline = res.data.TimelineList;
+  //     //取得した古いリストが空だったらreturn(lengthが上手く取れないのでこのような処理)
+  //     if (!oldList[0]) {
+  //       toast.success("過去の投稿はありません");
+  //       return;
+  //     }
+  //     //空でなければ既存のリストに追加
+  //     for (const oldData of oldList) {
+  //       timelineData.push(oldData);
+  //     }
+  //     toast.success("過去の投稿を読み込みました");
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, [loginId, timelineData]);
 
   //初期値エラー
-  if (!error && !timelineData) {
+  if (!error && !data) {
     return (
       <div className="flex justify-center pt-10 w-full">
         <div className="animate-spin h-8 w-8 bg-basic rounded-xl"></div>
@@ -112,84 +118,89 @@ const Timeline: NextPage = () => {
   //HTMLコーナー
   return (
     <>
-      {timelineData && (
-        <div>
-          {/* サブヘッダー */}
-          <SubHeader title="タイムライン" />
-          {/* タイムラインゾーン */}
-          <div className="text-center my-10 animate-bounce">
-            <Button
-              label="新しいつぶやきを読み込む"
-              size="lg"
-              onClick={() => {
-                updateData;
-                toast.success("新しい投稿を読み込みました");
-              }}
-            />
-          </div>
-          {timelineData.map((value, key) => (
-            <div key={key} className="flex border border-t-0 border-gray-200">
-              <div
-                className="rounded-full w-1/5 text-center pt-5 cursor-pointer hover:opacity-50"
-                onClick={() => {
-                  goUserPage(value.userId);
-                }}
-              >
-                <Image
-                  src={`/image/userIcon/${value.userPhotoPath}`}
-                  width={100}
-                  height={100}
-                  alt="icon"
-                  className="rounded-full"
-                />
-              </div>
-              <div className="w-4/5">
+      {/* サブヘッダー */}
+      <SubHeader title="タイムライン" />
+      {/* タイムラインゾーン */}
+      <div className="text-center my-10 animate-bounce">
+        <Button
+          label="新しいつぶやきを読み込む"
+          size="lg"
+          onClick={() => {
+            // updateData;
+            toast.success("新しい投稿を読み込みました");
+          }}
+        />
+      </div>
+      {data &&
+        // dataはページごとの連想配列の配列
+        data.map((pageData) =>
+          pageData?.TimelineList.map((timelime: any) => {
+            return (
+              <div key={timelime.id} className="flex border border-t-0 border-gray-200">
                 <div
-                  className="cursor-pointer hover:opacity-50"
+                  className="rounded-full w-1/5 text-center pt-5 cursor-pointer hover:opacity-50"
                   onClick={() => {
-                    goDetailPage(value.id);
+                    goUserPage(timelime.userId);
                   }}
                 >
-                  <div className="text-xl font-extrabold pt-3 pb-3">
-                    {value.accountName}
-                  </div>
-                  <div className="pt-5 pb-5 pl-5 w-8/12">{value.sentence}</div>
+                  <Image
+                    src={`/image/userIcon/${timelime.userPhotoPath}`}
+                    width={100}
+                    height={100}
+                    alt="icon"
+                    className="rounded-full"
+                  />
                 </div>
+                <div className="w-4/5">
+                  <div
+                    className="cursor-pointer hover:opacity-50"
+                    onClick={() => {
+                      goDetailPage(timelime.id);
+                    }}
+                  >
+                    <div className="text-xl font-extrabold pt-3 pb-3">
+                      {timelime.accountName}
+                    </div>
+                    <div className="pt-5 pb-5 pl-5 w-8/12">
+                      {timelime.sentence}
+                    </div>
+                  </div>
 
-                <div className="w-full text-right py-3">
-                  <CommentIcon
-                    commentCount={value.commentCount}
-                    postId={value.id}
-                    target="timeline"
-                    success={updateData}
-                  />
-                  <FavoBtn
-                    postId={value.id}
-                    favoCount={value.likeCount}
-                    isFavo={value.myLike}
-                    type="タイムライン"
-                    success={updateData}
-                  />
-                  <TrashBtn
-                    postId={value.id}
-                    type="タイムライン"
-                    success={updateData}
-                  />
+                  <div className="w-full text-right py-3">
+                    <CommentIcon
+                      commentCount={timelime.commentCount}
+                      postId={timelime.id}
+                      target="timeline"
+                      success={updateData}
+                    />
+                    <FavoBtn
+                      postId={timelime.id}
+                      favoCount={timelime.likeCount}
+                      isFavo={timelime.myLike}
+                      type="タイムライン"
+                      success={updateData}
+                    />
+                    <TrashBtn
+                      postId={timelime.id}
+                      type="タイムライン"
+                      success={updateData}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-          <div
-            className="text-text-brown text-center my-5 cursor-pointer hover:text-basic"
-            onClick={getOldData}
-          >
-            過去の投稿を見る…
-          </div>
-          <div>
-            <PostBtn success={updateData} />
-          </div>
-        </div>
-      )}
+            );
+          }),
+        )}
+
+      <div
+        className="text-text-brown text-center my-5 cursor-pointer hover:text-basic"
+        onClick={loadMoreTimeline}
+      >
+        過去の投稿を見る…
+      </div>
+      <div>
+        <PostBtn success={updateData} />
+      </div>
     </>
   );
 };
