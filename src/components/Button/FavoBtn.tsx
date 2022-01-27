7b82c702acb826711b765c81503186dbd75ef7f2
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-27 14:46:26 +0900

    投稿にいいねをする機能のカスタムフックを作成

--- src/components/Button/FavoBtn.tsx
+++ src/components/Button/FavoBtn.tsx
@@ -1,20 +1,18 @@
-import { FC, memo, useCallback, useContext } from "react";
-import axios from "axios";
+import { FC, memo, useContext } from "react";
 
 import { loginIdContext } from "../../providers/LoginIdProvider";
-import { JAVA_API_URL } from "../../utils/const";
+import { useFavo } from "../../hooks/useFavo";
 
-//一旦?にしているけれど必須にしていいかも
 type Props = {
-  postId?: number; //投稿ID
-  favoCount?: number; //お気に入り数
-  isFavo?: boolean; //お気に入りしているかどうか
-  type?: string; //レビューかつぶやきか
-  success?: () => void; //自動リロード用メソッド
+  postId: number; //投稿ID
+  favoCount: number; //お気に入り数
+  isFavo: boolean; //お気に入りしているかどうか
+  type: string; //レビューかつぶやきか
+  success: () => void; //自動リロード用メソッド
 };
 
 /**
- * つぶやきをお気に入りに登録するボタン.(500エラー中)
+ * つぶやきをお気に入りに登録するボタン.
  */
 export const FavoBtn: FC<Props> = memo((props) => {
   //ログインID
@@ -23,64 +21,8 @@ export const FavoBtn: FC<Props> = memo((props) => {
   //props
   const { postId, favoCount, isFavo, type, success } = props;
 
-  /**
-   * ボタン押下でいいね発動.
-   */
-  const favo = useCallback(
-    async (e) => {
-      // 親要素へのイベントを伝播させない
-      e.stopPropagation();
-      try {
-        if (type === "タイムライン") {
-          //タイムラインに対するいいね
-          const res = await axios.post(`${JAVA_API_URL}/timeline/like`, {
-            timelineId: postId, //投稿ID
-            userLogicalId: hash, //ログインユーザID
-          });
-          if (res.data.status === "success") {
-            //リロード
-            if (success) {
-              success();
-            }
-          }
-        }
-
-        if (type === "タイムラインコメント") {
-          const res = await axios.post(
-            `${JAVA_API_URL}/timeline/comment/like`,
-            {
-              commentId: postId, //投稿ID
-              userLogicalId: hash, //ログインユーザID
-            },
-          );
-          if (res.data.status === "success") {
-            //リロード
-            if (success) {
-              success();
-            }
-          }
-        }
-
-        if (type === "レビュー") {
-          const res = await axios.post(`${JAVA_API_URL}/review/like`, {
-            reviewId: postId, //投稿ID
-            userLogicalId: hash, //ログインユーザID
-          });
-          if (res.data.status === "success") {
-            //リロード
-            if (success) {
-              success();
-            }
-          } else {
-            console.log(res.data.message);
-          }
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    [success, hash, postId, type],
-  );
+  // カスタムフックを使用する
+  const { favo } = useFavo(postId, type, success, hash);
 
   return (
     <>
