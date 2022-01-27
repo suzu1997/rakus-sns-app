edf9d47efa4ed718e14dd143f0929c49b3c9c9f7
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-01-27 16:09:33 +0900

    カスタムフックス化による削除

--- src/pages/auth/forgetpass.tsx
+++ src/pages/auth/forgetpass.tsx
@@ -1,87 +1,23 @@
 import { NextPage } from "next";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
 import { TextInput } from "../../components/Form/TextInput";
 import { Button } from "../../components/Button/Button";
 import { ConfModal } from "../../components//Modal/ConfModal";
-import { useState, useCallback } from "react";
-import { JAVA_API_URL } from "../../utils/const";
-import axios from "axios";
-
-//バリデーションチェック
-const schema = yup.object().shape({
-  //メールアドレスのバリデーション
-  email: yup
-    .string()
-    .required("メールアドレスを入力してください")
-    .email("メールアドレス形式で入力してください")
-    .max(255, "メールアドレスは255文字以内で入力してください"),
-});
+import { useForgetPass } from "../../hooks/useForgetPass";
 
 /**
  * パスワードを忘れたときの画面.
  * @returns パスワードを忘れたときの画面
  */
 const ForgetPass: NextPage = () => {
-  // モーダルのオープン状態
-  const [isOpen, setIsOpen] = useState(false);
-
   const {
     register,
     handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm({
-    //バリデーション機能を呼び出し
-    resolver: yupResolver(schema),
-  });
-
-  /**
-   * 送信ボタンを押した時のメソッド.
-   * @param data 入力したデータ
-   */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const onSubmit = async (data: any) => {
-    //APIに送るデータ
-    const postDate = {
-      email: data.email,
-    };
-    try {
-      const res = await axios.post(
-        `${JAVA_API_URL}/password/sendMail`,
-        postDate,
-      );
-      //メール認証に成功した場合
-      if (res.data.status === "success") {
-        //メール認証成功したらモーダルを開ける
-        setIsOpen(true);
-      } else {
-        alert(res.data.message);
-      }
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  //モーダルを閉じるメソッド.
-  const closeModal = useCallback(() => {
-    setIsOpen(false);
-  }, []);
-
-  //入力内容をクリアしてモーダルを閉じる
-  const doOnButton = () => {
-    //入力値をクリア
-    clear();
-    setIsOpen(false);
-  };
-
-  //入力データをクリア
-  const clear = () => {
-    reset({
-      email: "",
-    });
-  };
+    errors,
+    onSubmit,
+    closeModal,
+    doOnButton,
+    isOpen,
+  } = useForgetPass();
 
   return (
     <>
