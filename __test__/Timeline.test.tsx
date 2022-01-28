0aa527863f7370f7cb8cffc1b74088321b21c995
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-01-28 17:31:21 +0900

    整理

--- __test__/Timeline.test.tsx
+++ __test__/Timeline.test.tsx
@@ -1,10 +1,10 @@
 /**
  * @jest-environment jsdom
  */
-import "@testing-library/jest-dom/extend-expect";
-import TimelinePage from "../src/pages/timeline/index";
-import { TimelineDetailPage } from "../src/components/Timeline/TimelineDetail";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import { TimelineDetailPage } from "../src/components/Timeline/TimelineDetail";
 import { Timeline } from "../src/types/type";
 
 // describeでテストのタイトルを定義
@@ -15,28 +15,27 @@ describe("Timeline Page Test", () => {
   };
   let dummyProps: Props;
 
+  const timelineData = {
+    id: 1,
+    userId: 1,
+    accountName: "佐藤",
+    userPhotoPath: "user1.jpeg",
+    sentence: "テスト投稿",
+    likeCount: 3,
+    commentCount: 5,
+    updatedTime: "2020-01-01",
+    postedTime: "2000-10-10",
+    deleted: false,
+    myLike: false,
+  };
+
+  const successData = () => {
+    alert("成功");
+  };
+
   // itでテストケースを定義
   it("タイムライン詳細ページが上手く表示されるか", () => {
     // ダミーのpropsを定義
-
-    const timelineData = {
-      id: 1,
-      userId: 1,
-      accountName: "佐藤",
-      userPhotoPath: "user1.jpeg",
-      sentence: "テスト投稿",
-      likeCount: 3,
-      commentCount: 5,
-      updatedTime: "2020-01-01",
-      postedTime: "2000-10-10",
-      deleted: false,
-      myLike: false,
-    };
-
-    const successData = () => {
-      alert("成功");
-    };
-
     dummyProps = {
       detailData: timelineData,
       success: successData,
