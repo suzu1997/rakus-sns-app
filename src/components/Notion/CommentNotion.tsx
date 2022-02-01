69b9878b6838b10f286074b4ad01f893cca98caa
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-02-01 16:37:38 +0900

    渡す値を変更

--- src/components/Notion/CommentNotion.tsx
+++ src/components/Notion/CommentNotion.tsx
@@ -5,11 +5,11 @@ import Image from "next/image";
 
 //コメント数・対象の投稿IDを受け取る
 export type Props = {
-  notificationList: Array<notion>;
+  notification: notion;
 };
 
 export const CommentNotion: FC<Props> = (props) => {
-  const { notificationList } = props;
+  const { notification } = props;
 
   //1人1人のつぶやきの下に入る線
   const style = {
@@ -19,42 +19,41 @@ export const CommentNotion: FC<Props> = (props) => {
   //コメント→いいねを表示
   return (
     <>
-      {notificationList.map((value, key) => (
-        <div style={style} key={key} className="p-5 ml-10">
-          <Link href={`/user/${value.id}`}>
-            <a>
-              <div className="flex">
-                <span className="text-2xl text-red-500 mt-10">
-                  <i className="fas fa-heart"></i>
-                </span>
-                <span className="ml-3 cursor-pointer hover:opacity-50">
-                  <Link href={`/user/${value.userId}`}>
-                    <a>
-                      <Image
-                        src={`/image/userIcon/${value.userPhotoPath}`}
-                        width={100}
-                        height={100}
-                        alt="icon"
-                        className="rounded-full"
-                      />
-                    </a>
-                  </Link>
-                </span>
+      <div style={style} className="p-5 ml-10">
+        <Link href={`/user/${notification.id}`}>
+          <a>
+            <div className="flex">
+              <span className="text-2xl text-red-500 mt-10">
+                <i className="fas fa-heart"></i>
+              </span>
+              <span className="ml-3 cursor-pointer hover:opacity-50">
+                <Link href={`/user/${notification.userId}`}>
+                  <a>
+                    <Image
+                      src={`/image/userIcon/${notification.userPhotoPath}`}
+                      width={100}
+                      height={100}
+                      alt="icon"
+                      className="rounded-full"
+                    />
+                  </a>
+                </Link>
+              </span>
+            </div>
+            <div className=" cursor-pointer hover:opacity-50">
+              <div className="text-xl pt-3 pb-3 ml-16">
+                <>
+                  {notification.accountName}
+                  さんがあなたのコメントにいいねしました
+                  <div className="pt-5 pb-5 pl-5 w-8/12 ml-20 text-text-brown">
+                    {notification.parentCommentSentence}
+                  </div>
+                </>
               </div>
-              <div className=" cursor-pointer hover:opacity-50">
-                <div className="text-xl pt-3 pb-3 ml-16">
-                  <>
-                    {value.accountName}さんがあなたのコメントにいいねしました
-                    <div className="pt-5 pb-5 pl-5 w-8/12 ml-20 text-text-brown">
-                      {value.parentCommentSentence}
-                    </div>
-                  </>
-                </div>
-              </div>
-            </a>
-          </Link>
-        </div>
-      ))}
+            </div>
+          </a>
+        </Link>
+      </div>
     </>
   );
 };
