266a0beb183ba9223b9e7df8a1cc88884851c12c
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-02-01 16:38:05 +0900

    渡す値を変更

--- src/components/Notion/ReviewNotion.tsx
+++ src/components/Notion/ReviewNotion.tsx
@@ -5,11 +5,11 @@ import Image from "next/image";
 
 //コメント数・対象の投稿IDを受け取る
 export type Props = {
-  notificationList: Array<notion>;
+  notification: notion;
 };
 
 export const ReviewNotion: FC<Props> = (props) => {
-  const { notificationList } = props;
+  const { notification } = props;
 
   //1人1人のつぶやきの下に入る線
   const style = {
@@ -19,62 +19,61 @@ export const ReviewNotion: FC<Props> = (props) => {
   //レビュー→コメント／いいねを表示
   return (
     <>
-      {notificationList.map((value, key) => (
-        <div style={style} key={key} className="p-5 ml-10">
-          <Link href={`/user/${value.id}`}>
-            <a>
-              <div className="flex">
-                {/* いいねの場合ハートを表示 */}
-                {value.like && (
-                  <span className="text-2xl text-red-500 mt-10">
-                    <i className="fas fa-heart"></i>
-                  </span>
-                )}
-                {/* コメントの場合ふきだしを表示 */}
-                {value.comment && (
-                  <span className="text-3xl text-yellow-600 mt-10">
-                    <i className="fas fa-comment"></i>
-                  </span>
-                )}
-                <span className="ml-3 cursor-pointer hover:opacity-50">
-                  <Link href={`/user/${value.userId}`}>
-                    <a>
-                      <Image
-                        src={`/image/userIcon/${value.userPhotoPath}`}
-                        width={100}
-                        height={100}
-                        alt="icon"
-                        className="rounded-full"
-                      />
-                    </a>
-                  </Link>
+      <div style={style} className="p-5 ml-10">
+        <Link href={`/user/${notification.id}`}>
+          <a>
+            <div className="flex">
+              {/* いいねの場合ハートを表示 */}
+              {notification.like && (
+                <span className="text-2xl text-red-500 mt-10">
+                  <i className="fas fa-heart"></i>
                 </span>
+              )}
+              {/* コメントの場合ふきだしを表示 */}
+              {notification.comment && (
+                <span className="text-3xl text-yellow-600 mt-10">
+                  <i className="fas fa-comment"></i>
+                </span>
+              )}
+              <span className="ml-3 cursor-pointer hover:opacity-50">
+                <Link href={`/user/${notification.userId}`}>
+                  <a>
+                    <Image
+                      src={`/image/userIcon/${notification.userPhotoPath}`}
+                      width={100}
+                      height={100}
+                      alt="icon"
+                      className="rounded-full"
+                    />
+                  </a>
+                </Link>
+              </span>
+            </div>
+            <div className=" cursor-pointer hover:opacity-50">
+              <div className="text-xl pt-3 pb-3 ml-16">
+                {notification.like && (
+                  <>
+                    {notification.accountName}
+                    さんがあなたのレビュー投稿にいいねしました
+                    <div className="pt-5 pb-5 pl-5 w-8/12 ml-20 text-text-brown">
+                      {notification.reviewSentence}
+                    </div>
+                  </>
+                )}
+                {notification.comment && (
+                  <>
+                    {notification.accountName}
+                    さんがあなたのレビュー投稿にコメントしました
+                    <div className="py-5 w-8/12 ml-20 text-text-brown">
+                      {notification.comment}
+                    </div>
+                  </>
+                )}
               </div>
-              <div className=" cursor-pointer hover:opacity-50">
-                <div className="text-xl pt-3 pb-3 ml-16">
-                  {value.like && (
-                    <>
-                      {value.accountName}さんがあなたの投稿にいいねしました
-                      <div className="pt-5 pb-5 pl-5 w-8/12 ml-20 text-text-brown">
-                        {value.reviewSentence}
-                      </div>
-                    </>
-                  )}
-                  {value.comment && (
-                    <>
-                      {value.accountName}
-                      さんがあなたのレビュー投稿にコメントしました
-                      <div className="py-5 w-8/12 ml-20 text-text-brown">
-                        {value.comment}
-                      </div>
-                    </>
-                  )}
-                </div>
-              </div>
-            </a>
-          </Link>
-        </div>
-      ))}
+            </div>
+          </a>
+        </Link>
+      </div>
     </>
   );
 };
