5725541aa15036406fde0caf878d9229484304d1
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-02-02 17:42:42 +0900

    メソッド部分をコンポーネントに移動

--- src/components/User/TimelineHisCard.tsx
+++ src/components/User/TimelineHisCard.tsx
@@ -1,13 +1,82 @@
 import { FC, memo } from "react";
+import { useCallback, useContext } from "react";
+import { useRouter } from "next/router";
 import Image from "next/image";
+import useSWR from "swr";
+
+import { loginIdContext } from "../../providers/LoginIdProvider";
+import { Timeline } from "../../types/type";
 import { CommentIcon } from "../Button/CommentIcon";
 import { FavoBtn } from "../Button/FavoBtn";
 import { TrashBtn } from "../Button/TrashBtn";
+import { JAVA_API_URL } from "../../utils/const";
 import { getFormattedDate } from "../../utils/methods";
+
+type Props = Timeline & {
+  type: string;
+};
+
 /**
  * タイムライン履歴を表示するカードコンポーネント
  */
 export const TimelineHisCard: FC<Props> = memo((props) => {
+  const {
+    id,
+    userId,
+    accountName,
+    userPhotoPath,
+    sentence,
+    likeCount,
+    commentCount,
+    postedTime,
+    myLike,
+  } = props;
+
+  //ログインID
+  const { loginId } = useContext(loginIdContext);
+  const { hash } = useContext(loginIdContext);
+
+  //ルーターリンク
+  const router = useRouter();
+
+  /**
+   * 画像クリックで投稿ユーザ情報ページに飛ぶ.
+   * @param userId - 投稿者ID
+   */
+  const goUserPage = useCallback(
+    (userId: number) => {
+      router.push(`/user/${userId}`);
+    },
+    [router],
+  );
+
+  /**
+   * 押下投稿の詳細に画面遷移.
+   * @remarks 受け取った記事IDの詳細画面に遷移
+   */
+  const goDetailTimelinePage = useCallback(
+    (postId: number) => {
+      router.push(`/timeline/${postId}`);
+      // router.push(`/lunch/review/${postId}`);
+    },
+    [router],
+  );
+
+  /**
+   * APIで初期表示用データ取得.
+   */
+  const { mutate } = useSWR(`${JAVA_API_URL}/user/${userId}/${hash}`);
+
+  /**
+   * 履歴表示一覧の情報を更新するメソッド.
+   *
+   * @remarks
+   * 成功すると呼ばれる。
+   */
+  const updateData = useCallback(() => {
+    mutate(); // 履歴一覧を再検証・再取得する
+  }, [mutate]);
+
   return (
     <div
       key={id}
