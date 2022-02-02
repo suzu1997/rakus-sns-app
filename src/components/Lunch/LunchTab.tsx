284a6f38994fdeeb5f2a9afb31d4eaf781f7a812
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-02-03 08:34:31 +0900

    pathをSSRで取得するようにしたら、タブのリロード問題が解決した

--- src/components/Lunch/LunchTab.tsx
+++ src/components/Lunch/LunchTab.tsx
@@ -1,4 +1,5 @@
 import { FC, memo } from "react";
+import { useRouter } from "next/router";
 import { Tab } from "@headlessui/react";
 
 import { ReviewList } from "./ReviewList";
@@ -24,42 +25,60 @@ const tabMap = [
 /**
  * ランチのレビューと店情報の切り替え用タブ.
  */
-export const LunchTab: FC = memo(() => {
-  return (
-    <div className="w-full">
-      <Tab.Group>
-        <Tab.List className="flex p-1 space-x-1 bg-bgc rounded-xl">
-          {tabMap.map((tab) => (
-            <Tab
-              key={tab.path}
-              className={({ selected }) =>
-                classNames(
-                  "w-full py-2.5 text-sm leading-5 font-bold text-text-brown rounded-lg",
-                  "focus:outline-none focus:ring-2 ring-offset-2 ring-white ring-opacity-60",
-                  selected
-                    ? "bg-white shadow border-4 border-basic"
-                    : "text-text-brown text-opacity-60 hover:text-opacity-100 hover:bg-white hover:bg-opacity-60 ",
-                )
-              }
-            >
-              {tab.display}
-            </Tab>
-          ))}
-        </Tab.List>
-        <Tab.Panels>
-          <Tab.Panel>
-            {/* タブがランチレビューの時はレビュー一覧表示 */}
-            <ReviewList />
-          </Tab.Panel>
-          <Tab.Panel>
-            {/* タブがお店情報の時は店一覧表示 */}
-            <div className="flex flex-col-reverse items-center lg:flex-row lg:items-start">
-              <RestaurantList />
-              <LunchSearchArea />
-            </div>
-          </Tab.Panel>
-        </Tab.Panels>
-      </Tab.Group>
-    </div>
-  );
-});
+export const LunchTab: FC<{ path: string }> = memo(
+  ({ path }: { path: string }) => {
+    const router = useRouter();
+    return (
+      <div className="w-full">
+        <Tab.Group
+          onChange={(idx) => {
+            // タブが変更されたらrouterへpush。
+            router.push(
+              {
+                pathname: router.pathname,
+                query: {
+                  path: [tabMap[idx].path],
+                },
+              },
+              undefined,
+              { shallow: true },
+            ); // shallowすることでrouterを再度呼び出さない
+          }}
+          defaultIndex={path === "restaurant" ? 1 : 0}
+        >
+          <Tab.List className="flex p-1 space-x-1 bg-bgc rounded-xl">
+            {tabMap.map((tab) => (
+              <Tab
+                key={tab.path}
+                className={({ selected }) =>
+                  classNames(
+                    "w-full py-2.5 text-sm leading-5 font-bold text-text-brown rounded-lg",
+                    "focus:outline-none focus:ring-2 ring-offset-2 ring-white ring-opacity-60",
+                    selected
+                      ? "bg-white shadow border-4 border-basic"
+                      : "text-text-brown text-opacity-60 hover:text-opacity-100 hover:bg-white hover:bg-opacity-60 ",
+                  )
+                }
+              >
+                {tab.display}
+              </Tab>
+            ))}
+          </Tab.List>
+          <Tab.Panels>
+            <Tab.Panel>
+              {/* タブがランチレビューの時はレビュー一覧表示 */}
+              <ReviewList />
+            </Tab.Panel>
+            <Tab.Panel>
+              {/* タブがお店情報の時は店一覧表示 */}
+              <div className="flex flex-col-reverse items-center lg:flex-row lg:items-start">
+                <RestaurantList />
+                <LunchSearchArea />
+              </div>
+            </Tab.Panel>
+          </Tab.Panels>
+        </Tab.Group>
+      </div>
+    );
+  },
+);
