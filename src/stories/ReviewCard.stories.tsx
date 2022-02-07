e34163582b22caddcb4921c0fe77b1c77c0b4dab
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-02-07 15:14:33 +0900

    レビューカードコンポーネントのストーリーを追加

--- src/stories/ReviewCard.stories.tsx
+++ src/stories/ReviewCard.stories.tsx
@@ -14,15 +14,15 @@ export default {
 } as ComponentMeta<typeof ReviewCard>;
 
 // 名前付きエクスポートはデフォルトでストーリーオブジェクトを表す
-export const Default: ComponentStoryObj<typeof ReviewCard> = {
+export const ListWithRestaurantLink: ComponentStoryObj<typeof ReviewCard> = {
   args: {
     accountName: "Suzu",
     userPhotoPath: "user3.jpeg",
     restaurantId: 1,
     restaurantName: "らーめん はやし田",
-    restaurantPhotoPath: "/restaurant1.jpg",
+    restaurantPhotoPath: "https://imgfp.hotp.jp/IMGH/81/98/P038318198/P038318198_168.jpg",
     star: 4.5,
-    sentence: "鴨と大山鶏の奥深いスープ「はやし田」",
+    sentence: "おいしかった！",
     likeCount: 30,
     commentCount: 5,
     postedTime: new Date("2022-01-21 22:31:29"),
@@ -30,5 +30,62 @@ export const Default: ComponentStoryObj<typeof ReviewCard> = {
     type: "一覧",
     hasRestaurantInfo: true,
   },
-  // storyName: "デフォルト",
+  storyName: "一覧(店舗情報あり)",
+};
+
+export const ListWithoutRestaurantLink: ComponentStoryObj<typeof ReviewCard> = {
+  args: {
+    accountName: "Suzu",
+    userPhotoPath: "user3.jpeg",
+    restaurantId: 1,
+    restaurantName: "らーめん はやし田",
+    restaurantPhotoPath: "https://imgfp.hotp.jp/IMGH/81/98/P038318198/P038318198_168.jpg",
+    star: 2.5,
+    sentence: "おいしかった！",
+    likeCount: 30,
+    commentCount: 5,
+    postedTime: new Date("2022-01-21 22:31:29"),
+    myLike: true,
+    type: "一覧",
+    hasRestaurantInfo: false,
+  },
+  storyName: "一覧(店舗情報なし)",
+};
+
+export const DetailWithRestaurantLink: ComponentStoryObj<typeof ReviewCard> = {
+  args: {
+    accountName: "Suzu",
+    userPhotoPath: "user3.jpeg",
+    restaurantId: 1,
+    restaurantName: "らーめん はやし田",
+    restaurantPhotoPath: "https://imgfp.hotp.jp/IMGH/81/98/P038318198/P038318198_168.jpg",
+    star: 2.5,
+    sentence: "おいしかった！",
+    likeCount: 30,
+    commentCount: 5,
+    postedTime: new Date("2022-01-21 22:31:29"),
+    myLike: false,
+    type: "詳細",
+    hasRestaurantInfo: true,
+  },
+  storyName: "詳細(店舗情報あり)",
+};
+
+export const DetailWithoutRestaurantLink: ComponentStoryObj<typeof ReviewCard> = {
+  args: {
+    accountName: "Suzu",
+    userPhotoPath: "user3.jpeg",
+    restaurantId: 1,
+    restaurantName: "らーめん はやし田",
+    restaurantPhotoPath: "https://imgfp.hotp.jp/IMGH/81/98/P038318198/P038318198_168.jpg",
+    star: 2.5,
+    sentence: "おいしかった！",
+    likeCount: 30,
+    commentCount: 5,
+    postedTime: new Date("2022-01-21 22:31:29"),
+    myLike: false,
+    type: "詳細",
+    hasRestaurantInfo: false,
+  },
+  storyName: "詳細(店舗情報なし)",
 };
