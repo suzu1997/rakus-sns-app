94b54df6da9cbb3f1ddc75d3ece4b5a01b81e913
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-02-07 11:52:35 +0900

    セレクトボックスのストーリー書き換えとコンポーネント修正

--- src/stories/SelectBox.stories.tsx
+++ src/stories/SelectBox.stories.tsx
@@ -1,22 +1,24 @@
-import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import type {
+  ComponentMeta,
+  ComponentStoryObj,
+} from "@storybook/react";
 import { SelectBox } from "../components/Form/SelectBox";
 
 export default {
-  title: "SelectBox",
   component: SelectBox,
 } as ComponentMeta<typeof SelectBox>;
 
-const Template: ComponentStory<typeof SelectBox> = (args) => <SelectBox {...args} />;
-
-export const Default: ComponentStory<typeof SelectBox> = Template.bind({});
-Default.args = {
-  label: "Default",
-  selectedOption: { id: "1", name: "テスト1" },
-  options: [
-    { id: "1", name: "テスト1" },
-    { id: "2", name: "テスト2" },
-    { id: "3", name: "テスト3" },
-    { id: "4", name: "テスト4" },
-  ],
+export const Default: ComponentStoryObj<typeof SelectBox> = {
+  args: {
+    label: "Default",
+    selectedOption: { id: "1", name: "テスト1" },
+    options: [
+      { id: "1", name: "テスト1" },
+      { id: "2", name: "テスト2" },
+      { id: "3", name: "テスト3" },
+      { id: "4", name: "テスト4" },
+    ],
+    fullWidth: false,
+  },
+  storyName: "デフォルト",
 };
-Default.storyName = "デフォルト";
