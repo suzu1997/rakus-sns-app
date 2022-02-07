a11118f3c9784c522da9bdee92a7cf21ffc1de34
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-02-07 13:57:05 +0900

    @storybook/addon-interactionsを追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-postcss",
+    '@storybook/addon-interactions',
   ],
   framework: "@storybook/react",
 };
