af46c7035367bb5979d10e69bae74838eefd597d
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-02-07 15:45:41 +0900

    レビューカードのストーリーファイル修正と、Storybookの定義内容をテストで使用

--- src/stories/ReviewCard.stories.tsx
+++ src/stories/ReviewCard.stories.tsx
@@ -1,16 +1,8 @@
 import type { ComponentMeta, ComponentStoryObj } from "@storybook/react";
 import { ReviewCard } from "../components/Lunch/ReviewCard";
-import { action } from "@storybook/addon-actions";
 
 export default {
   component: ReviewCard,
-  // render: () => {
-  //   return (
-  //     <div className="max-w-xl w-5/6 mx-auto py-5">
-  //       <AddManuallyForm cansel={action("cansel")} />
-  //     </div>
-  //   );
-  // }
 } as ComponentMeta<typeof ReviewCard>;
 
 // 名前付きエクスポートはデフォルトでストーリーオブジェクトを表す
@@ -20,7 +12,8 @@ export const ListWithRestaurantLink: ComponentStoryObj<typeof ReviewCard> = {
     userPhotoPath: "user3.jpeg",
     restaurantId: 1,
     restaurantName: "らーめん はやし田",
-    restaurantPhotoPath: "https://imgfp.hotp.jp/IMGH/81/98/P038318198/P038318198_168.jpg",
+    restaurantPhotoPath:
+      "https://imgfp.hotp.jp/IMGH/81/98/P038318198/P038318198_168.jpg",
     star: 4.5,
     sentence: "おいしかった！",
     likeCount: 30,
@@ -35,57 +28,25 @@ export const ListWithRestaurantLink: ComponentStoryObj<typeof ReviewCard> = {
 
 export const ListWithoutRestaurantLink: ComponentStoryObj<typeof ReviewCard> = {
   args: {
-    accountName: "Suzu",
-    userPhotoPath: "user3.jpeg",
-    restaurantId: 1,
-    restaurantName: "らーめん はやし田",
-    restaurantPhotoPath: "https://imgfp.hotp.jp/IMGH/81/98/P038318198/P038318198_168.jpg",
-    star: 2.5,
-    sentence: "おいしかった！",
-    likeCount: 30,
-    commentCount: 5,
-    postedTime: new Date("2022-01-21 22:31:29"),
-    myLike: true,
-    type: "一覧",
-    hasRestaurantInfo: false,
+    ...ListWithRestaurantLink.args, // 共通部分
+    hasRestaurantInfo: false, // 変更部分
   },
   storyName: "一覧(店舗情報なし)",
 };
 
 export const DetailWithRestaurantLink: ComponentStoryObj<typeof ReviewCard> = {
   args: {
-    accountName: "Suzu",
-    userPhotoPath: "user3.jpeg",
-    restaurantId: 1,
-    restaurantName: "らーめん はやし田",
-    restaurantPhotoPath: "https://imgfp.hotp.jp/IMGH/81/98/P038318198/P038318198_168.jpg",
-    star: 2.5,
-    sentence: "おいしかった！",
-    likeCount: 30,
-    commentCount: 5,
-    postedTime: new Date("2022-01-21 22:31:29"),
-    myLike: false,
+    ...ListWithRestaurantLink.args,
     type: "詳細",
-    hasRestaurantInfo: true,
   },
   storyName: "詳細(店舗情報あり)",
 };
 
-export const DetailWithoutRestaurantLink: ComponentStoryObj<typeof ReviewCard> = {
-  args: {
-    accountName: "Suzu",
-    userPhotoPath: "user3.jpeg",
-    restaurantId: 1,
-    restaurantName: "らーめん はやし田",
-    restaurantPhotoPath: "https://imgfp.hotp.jp/IMGH/81/98/P038318198/P038318198_168.jpg",
-    star: 2.5,
-    sentence: "おいしかった！",
-    likeCount: 30,
-    commentCount: 5,
-    postedTime: new Date("2022-01-21 22:31:29"),
-    myLike: false,
-    type: "詳細",
-    hasRestaurantInfo: false,
-  },
-  storyName: "詳細(店舗情報なし)",
-};
+export const DetailWithoutRestaurantLink: ComponentStoryObj<typeof ReviewCard> =
+  {
+    args: {
+      ...ListWithoutRestaurantLink.args,
+      type: "詳細",
+    },
+    storyName: "詳細(店舗情報なし)",
+  };
