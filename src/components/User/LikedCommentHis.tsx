e0e537f3d34f03b2d98881d0bd94b8cabf180cc6
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-02-08 15:50:47 +0900

    さっきとほとんど同じことしてます

--- src/components/User/LikedCommentHis.tsx
+++ src/components/User/LikedCommentHis.tsx
@@ -71,40 +71,45 @@ export const LikedCommentHis: FC<CommentHis> = memo((props) => {
       key={id}
       className=" border border-t-1 mt-1 border-blue-100text-sm font-medium leading-5 focus:outline-none rounded-xl bg-white relative p-3 "
     >
-      <div
-        className="hover:bg-coolGray-100 cursor-pointer hover:opacity-50"
-        onClick={() => {
-          goDetailTimelinePage(id);
-        }}
-      >
-        <span className="text-xl font-extrabold pt-3 pb-3">{accountName}</span>
-        <span className="ml-7">
-          コメント日時:
-          {getFormattedDate(new Date(actionedTime))}
-        </span>
-      </div>
-      <div className="flex">
-        <span
-          className="rounded-full  pt-5 cursor-pointer hover:opacity-50"
-          onClick={() => {
-            goUserPage(userId);
-          }}
-        >
-          <Image
-            src={`/image/userIcon/${userPhotoPath}`}
-            width={100}
-            height={100}
-            alt="icon"
-            className="rounded-full"
-          />
-        </span>
-        <span
-          className="p-10 w-8/12 hover:bg-coolGray-100 cursor-pointer hover:opacity-50"
+      <div className="flex flex-col">
+        <div className="flex">
+          <div
+            className="lg:1/12 cursor-pointer hover:opacity-50"
+            onClick={() => {
+              goUserPage(userId);
+            }}
+          >
+            <Image
+              src={`/image/userIcon/${userPhotoPath}`}
+              width={80}
+              height={80}
+              alt="icon"
+              className="rounded-full"
+            />
+          </div>
+          <div
+            className="ml-4 hover:bg-coolGray-100 cursor-pointer hover:opacity-50"
+            onClick={() => {
+              goDetailTimelinePage(id);
+            }}
+          >
+            <div className="text-xl font-extrabold py-3">{accountName}</div>
+          </div>
+        </div>
+
+        <div
+          className="lg:ml-20 my-2 hover:bg-coolGray-100 cursor-pointer hover:opacity-50"
           onClick={() => {
             goDetailTimelinePage(id);
           }}
         >
           {comment}
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <span>
+          コメント日時:
+          {getFormattedDate(new Date(actionedTime))}
         </span>
       </div>
     </div>
