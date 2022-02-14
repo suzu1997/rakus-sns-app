90674734b5f921be0395100a6b45e5254e4baf77
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-02-14 12:27:05 +0900

    文章決めた

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,18 @@ import { NextPage } from "next";
  * @returns 存在しないURL/削除済投稿が叩かれた際に開く
  */
 const Error404Page: NextPage = () => {
-  return <></>;
+  return (
+    <div>
+      <div>404 This page could not be found.</div>
+      <div>ページが見つかりません</div>
+      <div>下記の可能性があります</div>
+      <ul>
+        <li>存在しないURL</li>
+        <li>情報が削除された</li>
+        <li>情報が存在しない</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Error404Page;
