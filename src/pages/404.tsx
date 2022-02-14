6123db6cceefae5ebcf4ca93e0cafab19278d8af
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-02-14 12:20:14 +0900

    404ページ作成

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+/**
+ * 404ページ.
+ * @returns 存在しないURL/削除済投稿が叩かれた際に開く
+ */
+const Error404Page: NextPage = () => {
+  return <></>;
+};
+
+export default Error404Page;
